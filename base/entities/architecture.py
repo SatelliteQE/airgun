f6539f387b39ae69e0316012fd73b521ef24ae7d
Oleksander <alexshtayer@yahoo.com>
2018-01-30 13:58:31 +0000

    Merged in oshtaier/airgun/test2 (pull request #4)
    hello world
    
    Approved-by: Balah <balakhtar.a@gmail.com>

--- base/entities/architecture.py
+++ base/entities/architecture.py
@@ -1,10 +1,12 @@
-from base.navigation import Navigator
+from base.navigation import BaseNavigator, navigator
 from widgetastic.widget import View, Text, TextInput
+
+import time
 
 
 class Architecture(View):
 
-    navigator = Navigator()
+    title = Text("//h1[text()='Architectures']")
     new = Text("//a[contains(@href, '/architectures/new')]")
     search = Text("//a[contains(., '{}')]")
     navigate_locator = "//a[@id='menu_item_architectures']"
@@ -12,17 +14,45 @@ class Architecture(View):
     name = TextInput(locator="//input[@id='architecture_name']")
     submit = Text('//input[@name="commit"]')
 
+    @property
+    def is_displayed(self):
+        return self.browser.wait_for_element(
+            self.title, exception=False) is not None
+
     # @View.nested
     # class General(View):
     #     name = TextInput('#architecture_name')
 
-    def navigate(self):
-        self.navigator.menu_click([
-            "//a[@id='hosts_menu']", self.navigate_locator])
+    NAVBAR_PATH = (
+        '//div[contains(@class,"navbar-inner") and '
+        'not(contains(@style, "display"))]')
+    MENU_CONTAINER_PATH = NAVBAR_PATH + '//ul[@id="menu"]'
+
+    def menu_click(self, tree):
+        for i, el in enumerate(tree, start=1):
+            locator = self.MENU_CONTAINER_PATH + el
+            self.browser.wait_for_element(locator)
+            self.browser.move_to_element(locator)
+            time.sleep(0.5)
+            if len(tree) == i:
+                self.browser.wait_for_element(locator)
+                self.browser.click(locator)
+                time.sleep(1)
 
     def create_architecture(self, values):
-        self.navigate()
+        navigator.navigate(self, 'All')
         self.browser.wait_for_element(self.new)
         self.browser.click(self.new)
         self.fill(values)
         self.browser.click(self.submit)
+
+
+@navigator.register(Architecture, 'All')
+class ShowAllArchitectures(BaseNavigator):
+    VIEW = Architecture
+
+    # prerequisite = NavigateToSibling('Dashboard')
+
+    def step(self):
+        self.obj.menu_click(
+            ["//a[@id='hosts_menu']", self.obj.navigate_locator])
--- base/navigation.py
+++ base/navigation.py
@@ -1,22 +1,17 @@
-import time
-from widgetastic.widget import View
+from navmazing import Navigate, NavigateStep
+from selenium.common.exceptions import NoSuchElementException
 
 
-class Navigator(View):
+class BaseNavigator(NavigateStep):
     """Quickly navigate through menus and tabs."""
 
-    NAVBAR_PATH = (
-        '//div[contains(@class,"navbar-inner") and '
-        'not(contains(@style, "display"))]')
-    MENU_CONTAINER_PATH = NAVBAR_PATH + '//ul[@id="menu"]'
+    VIEW = None
 
-    def menu_click(self, tree):
-        for i, el in enumerate(tree, start=1):
-            locator = self.MENU_CONTAINER_PATH + el
-            self.browser.wait_for_element(locator)
-            self.browser.move_to_element(locator)
-            time.sleep(0.5)
-            if len(tree) == i:
-                self.browser.wait_for_element(locator)
-                self.browser.click(locator)
-                time.sleep(1)
+    def am_i_here(self):
+        try:
+            return self.obj.is_displayed
+        except (AttributeError, NoSuchElementException):
+            return False
+
+
+navigator = Navigate()
