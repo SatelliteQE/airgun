ab1ffc23d10b4a07ab78f89601ab9497a1abc4ee
Andrii Balakhtar <balakhtara@gmail.com>
2018-01-31 18:19:33 +0200

    Created separate importable file for fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,8 @@
-import pytest
-
 from airgun import settings
-from airgun.session import Session
 
 
-@pytest.fixture(autouse=True)
-def session(request):
-    return Session(request.module.__name__)
-
-
+# todo: make sure this hook is needed - for unit tests configuring may be
+# redundant
 def pytest_collection_modifyitems():
     """ called after collection has been performed, may filter or re-order
     the items in-place.
