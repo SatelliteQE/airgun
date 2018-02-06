35f5e7170d5ab8b4661d37c7baf35cf15cb73ade
Andrii Balakhtar <balakhtara@gmail.com>
2018-02-06 13:40:59 +0200

    Register navigator to entities instead of views

--- airgun/entities/architecture.py
+++ airgun/entities/architecture.py
@@ -1,14 +1,40 @@
+from navmazing import NavigateToSibling
+
 from airgun.entities.base import BaseEntity
-from airgun.views.architecture import ArchitectureView
+from airgun.navigation import BaseNavigator, menu_click, navigator
+from airgun.views.architecture import ArchitectureView, ArchitectureDetailsView
 
 
 class ArchitectureEntity(BaseEntity):
 
     def create_architecture(self, values):
-        view = self.navigate_to(ArchitectureView, 'New')
+        view = self.navigate_to(self, 'New')
         view.fill(values)
         view.submit_data()
 
     def search(self, value):
-        view = self.navigate_to(ArchitectureView, 'All')
+        view = self.navigate_to(self, 'All')
         return view.search_element.search(value)
+
+
+@navigator.register(ArchitectureEntity, 'All')
+class ShowAllArchitectures(BaseNavigator):
+    VIEW = ArchitectureView
+
+    def step(self, *args, **kwargs):
+        menu_click(
+            ["//a[@id='hosts_menu']", self.view.navigate_locator],
+            self.view.browser
+        )
+
+
+@navigator.register(ArchitectureEntity, 'New')
+class AddNewArchitecture(BaseNavigator):
+    VIEW = ArchitectureDetailsView
+
+    prerequisite = NavigateToSibling('All')
+
+    def step(self, *args, **kwargs):
+        self.view.browser.wait_for_element(
+            self.parent.new, ensure_page_safe=True)
+        self.parent.browser.click(self.parent.new)
--- airgun/entities/login.py
+++ airgun/entities/login.py
@@ -1,14 +1,27 @@
 from airgun.entities.base import BaseEntity
+from airgun.navigation import BaseNavigator, navigator
 from airgun.views.login import LoginView
 
 
 class LoginEntity(BaseEntity):
 
     def login(self, values):
-        view = self.navigate_to(LoginView, 'NavigateToLogin')
+        view = self.navigate_to(self, 'NavigateToLogin')
         view.fill(values)
         self.browser.click(view.submit)
 
     def logout(self):
         # fixme: not implemented
         pass
+
+
+@navigator.register(LoginEntity)
+class NavigateToLogin(BaseNavigator):
+    VIEW = LoginView
+
+    def step(self, *args, **kwargs):
+        # fixme: logout() if logged_in
+        pass
+
+    def am_i_here(self, *args, **kwargs):
+        return self.view.is_displayed
