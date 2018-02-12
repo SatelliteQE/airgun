f0a11926b46d2add8742ff371062808f19dce99a
Andrii Balakhtar <balakhtara@gmail.com>
2018-02-12 18:55:52 +0200

    Added more docs

--- airgun/navigation.py
+++ airgun/navigation.py
@@ -1,15 +1,23 @@
-from cached_property import cached_property
+"""Airgun's implementation of base navigation and navigate steps."""
 import navmazing
+
+from cached_property import cached_property
 from selenium.common.exceptions import NoSuchElementException
 
 
-class BaseNavigator(navmazing.NavigateStep):
-    """Quickly navigate through menus and tabs."""
+class NavigateStep(navmazing.NavigateStep):
+    """Airgun's version of :class:`navmazing.NavigateStep` with custom
+    implementations of :meth:`am_i_here`, :meth:`go` and ability to work with
+    views.
+    """
 
     VIEW = None
 
     @cached_property
     def view(self):
+        """Cached property which returns instance of view, which was defined
+        for current navigate step by ``VIEW`` class attribute.
+        """
         if self.VIEW is None:
             raise AttributeError(
                 '{} does not have VIEW specified'.format(type(self).__name__))
@@ -17,6 +25,21 @@ class BaseNavigator(navmazing.NavigateStep):
                                 additional_context={'entity': self.obj})
 
     def create_view(self, view_class, additional_context=None):
+        """Method which creates an instance of view, defined by ``view_class``.
+
+        Created instance will have references to ``entity``,
+        :class:`airgun.browser.AirgunBrowser` and
+        :class:`airgun.session.Session` set as instance attributes
+        ``self.context['entity']``, ``self.browser`` and ``self.extra.session``
+        correspondingly.
+
+        :param widgetastic.widget.View view_class: class of view to create
+        :param dict optional additional_context: any additional context you
+            want to include. ``entity`` is set to current navigate step entity
+            by default
+        :return: instance of view
+        :rtype: widgetastic.widget.View
+        """
         if additional_context is None:
             additional_context = {}
         if not additional_context.get('entity'):
@@ -25,22 +48,53 @@ class BaseNavigator(navmazing.NavigateStep):
             self.navigate_obj.browser, additional_context=additional_context)
 
     def am_i_here(self, *args, **kwargs):
+        """Describes if the navigation is already at the requested destination.
+
+        By default, airgun relies on view's ``is_displayed`` property to
+        determine whether navigation succeeded. This method may be overridden
+        on specific entity's NavigateStep level for more complex logic if
+        needed.
+
+        :return: whether navigator is at requested destination or not.
+        :rtype: bool
+        """
         try:
             return self.view.is_displayed
         except (AttributeError, NoSuchElementException):
             return False
 
     def go(self, _tries=0, *args, **kwargs):
-        super(BaseNavigator, self).go(_tries=_tries, *args, **kwargs)
+        """Wrapper around :meth:`navmazing.NavigateStep.go` which returns
+        instance of view after successful navigation flow.
+
+        :return: view instance if class attribute ``VIEW`` is set or ``None``
+            otherwise
+        """
+        super(NavigateStep, self).go(_tries=_tries, *args, **kwargs)
         view = self.view if self.VIEW is not None else None
         return view
 
 
 class Navigate(navmazing.Navigate):
+    """Wrapper around :class:`navmazing.Navigate` which adds airgun browser as
+    class attribute.
+
+    For more information about :class:`Navigate` please refer to
+    `navmazing docs<https://github.com/RedHatQE/navmazing>`_.
+    """
 
     def __init__(self, browser=None):
+        """Store airgun browser instance to be able to perform navigation
+        steps.
+
+        :param airgun.browser.AirgunBrowser browser: airgun browser instance to
+            perform navigation with
+        """
         super(Navigate, self).__init__()
         self.browser = browser
 
 
+# Navigator instance to be used in other modules. Please note that you should
+# use it only for registering navigation steps, not for actual navigation. For
+# navigation use :attr:`airgun.Session.navigate_to` instead.
 navigator = Navigate()
--- airgun/session.py
+++ airgun/session.py
@@ -1,7 +1,10 @@
+"""Session controller which manages UI session"""
 import logging
 import os
 
 from datetime import datetime
+
+from fauxfactory import gen_string
 
 from airgun import settings
 from airgun.browser import AirgunBrowser, SeleniumBrowserFactory
@@ -16,17 +19,100 @@ LOGGER = logging.getLogger(__name__)
 
 
 class Session(object):
-    """A session context manager that manages login and logout"""
+    """A session context manager which is a key controller in airgun.
 
-    def __init__(self, test_name, user=None, password=None):
-        self.test_name = test_name
+    It is responsible for initializing and starting
+    :class:`airgun.browser.AirgunBrowser`, navigating to satellite, performing
+    post-init browser tweaks, initializing navigator, all available UI
+    entities, and logging in to satellite.
+
+    When session is about to close, it saves a screenshot in case of any
+    exception, attempts to log out from satellite and performs all necessary
+    browser closure steps like quitting the browser, sending results to
+    saucelabs, stopping docker container etc.
+
+    For tests level it offers direct control over when UI session is started
+    and stopped as well as provides all the entities available without the need
+    to import and initialize them manually.
+
+    Usage::
+
+        def test_foo():
+            # steps executed before starting UI session
+            # [...]
+
+            # start of UI session
+            with Session('test_foo') as session:
+                # steps executed during UI session. For example:
+                session.architecture.create({'name': 'bar'})
+                [...]
+
+            # steps executed after UI session was closed
+
+    You can also pass credentials different from default ones specified in
+    settings like that::
+
+        with Session(test_name, user='foo', password='bar'):
+            # [...]
+
+    Every test may use multiple sessions if needed::
+
+        def test_foo():
+            with Session('test_foo', 'admin', 'password') as admin_session:
+                # UI steps, performed by 'admin' user
+                admin_session.user.create({'login': 'user1', 'password: 'pwd'})
+                # [...]
+            with Session('test_foo', 'user1', 'pwd1') as user1_session:
+                # UI steps, performed by 'user1' user
+                user1_session.architecture.create({'name': 'bar'})
+                # [...]
+
+    Nested sessions are supported as well, just don't forget to use different
+    variables for sessions::
+
+        def test_foo():
+            with Session('test_foo', 'admin', 'password') as admin_session:
+                # UI steps, performed by 'admin' user only
+                admin_session.user.create({'login': 'user1', 'password: 'pwd'})
+                # [...]
+                with Session('test_foo', 'user1', 'pwd1') as user1_session:
+                    # UI steps, performed by 'user1' user OR 'admin' in
+                    # different browser instances
+                    user1_session.architecture.create({'name': 'bar'})
+                    admin_session.architecture.search('bar')
+                    # [...]
+                # UI steps, performed by 'admin' user only
+                admin_session.user.delete({'login': 'user1'})
+
+    """
+
+    def __init__(self, session_name=None, user=None, password=None):
+        """Stores provided values, doesn't perform any actions.
+
+        :param str optional session_name: string representing session name.
+            Used in screenshot names, saucelabs test results, docker container
+            names etc. You should provide your test name here in most cases.
+            Defaults to random alphanumeric value.
+        :param str optional user: username (login) of user, who will perform UI
+            actions. If None is passed - default one provided by settings will
+            beused.
+        :param str optional password: password for provided user. If None is
+            passed - default one from settings will be used.
+        """
+        self.name = session_name or gen_string('alphanumeric')
         self._user = user or settings.satellite.username
         self._password = password or settings.satellite.password
         self._factory = None
         self.browser = None
 
     def __enter__(self):
-        self._factory = SeleniumBrowserFactory(test_name=self.test_name)
+        """Starts the browser, navigates to satellite, performs post-init
+        browser tweaks, initializes navigator and UI entities, and logs in to
+        satellite.
+        """
+        LOGGER.info(
+            u'Starting UI session %r for user %r', self.name, self._user)
+        self._factory = SeleniumBrowserFactory(test_name=self.name)
         selenium_browser = self._factory.get_browser()
         selenium_browser.maximize_window()
         self.browser = AirgunBrowser(selenium_browser, self)
@@ -47,6 +133,14 @@ class Session(object):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        """Attempts to log out, saves the screenshot and performs all required
+        session closure activities.
+
+        NOTE: exceptions during logout or saving screenshot are just logged and
+            not risen not to shadow real session result.
+        """
+        LOGGER.info(
+            u'Stopping UI session %r for user %r', self.name, self._user)
         passed = True if exc_type is None else False
         try:
             if passed:
@@ -68,9 +162,10 @@ class Session(object):
         All directories will be created if they don't exist. Make sure that the
         user running robottelo have the right permissions to create files and
         directories matching the complete.
+
+        This method is called automatically in case any exception during UI
+        session happens.
         """
-        # Take a screenshot if any exception is raised and the test method is
-        # not in the skipped tests.
         now = datetime.now()
         path = os.path.join(
             settings.selenium.screenshots_path,
@@ -79,7 +174,7 @@ class Session(object):
         if not os.path.exists(path):
             os.makedirs(path)
         filename = '{0}-screenshot-{1}.png'.format(
-            self.test_name.replace(' ', '_'),
+            self.name.replace(' ', '_'),
             now.strftime('%Y-%m-%d_%H_%M_%S')
         )
         path = os.path.join(path, filename)
