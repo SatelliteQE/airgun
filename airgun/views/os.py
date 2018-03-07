f92dc0795b324aeb00e1d1b95b8978ccac57214c
Oleksandr Shtaier <alexshtayer@yahoo.com>
2018-03-07 17:27:32 +0200

    added tab func (#41)

--- airgun/views/os.py
+++ airgun/views/os.py
@@ -1,4 +1,5 @@
 from widgetastic.widget import GenericLocatorWidget, Text, TextInput, View
+from widgetastic_patternfly import Tab
 
 from airgun.views.common import BaseLoggedInView, SearchableViewMixin
 from airgun.widgets import CustomParameter, MultiSelect
@@ -25,29 +26,25 @@ class OperatingSystemDetailsView(BaseLoggedInView):
     architectures = MultiSelect(id='ms-operatingsystem_architecture_ids')
 
     @View.nested
-    class ptables(View):
-        view_tab = Text("//a[@href='#ptable']")
+    class ptables(Tab):
+        TAB_NAME = 'Partition Table'
         ptables = MultiSelect(id='ms-operatingsystem_ptable_ids')
 
         def fill(self, values):
-            self.browser.click(self.view_tab)
             self.ptables.fill(values)
 
         def read(self):
-            self.browser.click(self.view_tab)
             return self.ptables.read()
 
     @View.nested
-    class parameters(View):
-        view_tab = Text("//a[@href='#params']")
+    class parameters(Tab):
+        TAB_NAME = 'Parameters'
         params = CustomParameter()
 
         def fill(self, values):
-            self.browser.click(self.view_tab)
             self.params.fill(values)
 
         def read(self):
-            self.browser.click(self.view_tab)
             return self.params.read()
 
     @property
