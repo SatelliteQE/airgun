7e8b0f7d25c9b8d76ee49efa70438e36907a2c06
Oleksandr Shtaier <alexshtayer@yahoo.com>
2018-04-13 10:43:00 +0300

    Add LCE entity (#64)
    * Add LCE entity
    
    * Address comments

---
+++ airgun/entities/lifecycleenvironment.py
@@ -0,0 +1,87 @@
+from navmazing import NavigateToSibling
+
+from airgun.entities.base import BaseEntity
+from airgun.navigation import NavigateStep, navigator
+from airgun.views.lifecycleenvironment import (
+    LCECreateView,
+    LCEEditView,
+    LCEView,
+)
+
+
+class LCEEntity(BaseEntity):
+
+    def create_environment_path(self, values):
+        view = self.navigate_to(self, 'New Path')
+        view.fill(values)
+        view.submit.click()
+
+    def create_environment(self, values, entity_name):
+        view = self.navigate_to(
+            self, 'New Environment', entity_name=entity_name)
+        view.fill(values)
+        view.submit.click()
+
+    def create(self, values, prior_entity_name=None,):
+        """Create new lifecycle environment
+        :param values: Parameters to be assigned to lce, at least name should
+            be provided
+        :param prior_entity_name: Specify entity name which should be a parent
+            to lifecycle environment that created in a chain
+        """
+        if prior_entity_name is not None:
+            self.create_environment(values, prior_entity_name)
+        else:
+            self.create_environment_path(values)
+
+    def read(self):
+        view = self.navigate_to(self, 'All')
+        return view.read()
+
+    def update(self, values, entity_name='Library'):
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        return view.fill(values)
+
+
+@navigator.register(LCEEntity, 'All')
+class ShowAllLCE(NavigateStep):
+    VIEW = LCEView
+
+    def step(self, *args, **kwargs):
+        self.view.menu.select('Content', 'Lifecycle Environments')
+
+
+@navigator.register(LCEEntity, 'New Path')
+class AddNewEnvironmentPath(NavigateStep):
+    VIEW = LCECreateView
+
+    prerequisite = NavigateToSibling('All')
+
+    def step(self, *args, **kwargs):
+        self.parent.browser.click(self.parent.new_path)
+
+
+@navigator.register(LCEEntity, 'New Environment')
+class AddNewEnvironment(NavigateStep):
+    VIEW = LCECreateView
+
+    def prerequisite(self, *args, **kwargs):
+        return self.navigate_to(self.obj, 'All')
+
+    def step(self, *args, **kwargs):
+        self.parent.lce(kwargs.get('entity_name')).new_child.click()
+
+
+@navigator.register(LCEEntity, 'Edit')
+class EditEnvironment(NavigateStep):
+    VIEW = LCEEditView
+
+    def prerequisite(self, *args, **kwargs):
+        return self.navigate_to(self.obj, 'All')
+
+    def step(self, *args, **kwargs):
+        env_name = kwargs.get('entity_name')
+        if env_name == 'Library':
+            self.parent.edit_parent_env.click()
+        else:
+            self.parent.lce(kwargs.get('entity_name')).current_env.click()
---
+++ airgun/views/lifecycleenvironment.py
@@ -0,0 +1,108 @@
+from widgetastic.widget import (
+    ParametrizedView,
+    ParametrizedLocator,
+    Table,
+    Text,
+    TextInput,
+    View,
+)
+
+from airgun.views.common import BaseLoggedInView, SatTab
+from airgun.widgets import EditableEntry, ReadOnlyEntry
+
+
+class LCEView(BaseLoggedInView, ParametrizedView):
+    title = Text("//h2[contains(., 'Lifecycle Environment Paths')]")
+    new_path = Text(
+        "//a[contains(@href, '/lifecycle_environments') "
+        "and contains(@href, 'new') and contains(@class, 'btn-primary')]"
+    )
+    edit_parent_env = Text(
+        "//table[contains(@class, 'info-blocks')]"
+        "//a[contains(@ui-sref, 'environment.details')]"
+    )
+    parent_env_cvs_count = Text(
+        "//table[contains(@class, 'info-blocks')]"
+        "//td[span[contains(., 'Content Views')]]/div")
+    parent_env_products_count = Text(
+        "//table[contains(@class, 'info-blocks')]"
+        "//td[span[contains(., 'Products')]]/div")
+    parent_env_products_errata = Text(
+        "//table[contains(@class, 'info-blocks')]"
+        "//td[span[contains(., 'Errata')]]/div")
+
+    @property
+    def is_displayed(self):
+        return self.browser.wait_for_element(
+            self.title, exception=False) is not None
+
+    @View.nested
+    class lce(ParametrizedView):
+        ROOT = ParametrizedLocator(
+            ".//div[@ng-repeat='path in paths']"
+            "[table//th/a[normalize-space(.)='{lce_name}']]"
+        )
+        PARAMETERS = ('lce_name',)
+        LAST_ENV = "//div[@ng-repeat='path in paths']//table//th[last()]"
+        current_env = Text(ParametrizedLocator(
+            ".//a[normalize-space(.)='{lce_name}']"))
+        envs_table = Table(locator=".//table")
+        new_child = Text(".//a[contains(@href, '/lifecycle_environments/')]")
+
+        @classmethod
+        def all(cls, browser):
+            """Helper method which returns list of tuples with all available
+            LCE names (last available environment is used as a name). It's
+            required for :meth:`read` to work properly.
+            """
+            return [
+                (element.text,) for element in browser.elements(cls.LAST_ENV)]
+
+        def read(self):
+            """Returns content views and count hosts count per each available
+            lifecycle environment
+            We get dictionary in next format:
+
+                {
+                    'LCE_1': {'Content Views': 0, 'Content Hosts': 1},
+                    'LCE_2': {'Content Views': 1, 'Content Hosts': 2},
+                }
+
+            """
+            result = {}
+            available_envs = self.envs_table.headers[1:]
+            lce_metric_names = [row[0].text for row in self.envs_table]
+            for column_name in available_envs:
+                metric_values = (
+                    int(row[column_name].text) for row in self.envs_table)
+                result[column_name] = {}
+                for row_name in lce_metric_names:
+                    result[column_name][row_name] = next(metric_values)
+            return result
+
+
+class LCECreateView(BaseLoggedInView):
+    name = TextInput(id='name')
+    label = TextInput(id='label')
+    description = TextInput(id='description')
+    submit = Text("//button[contains(@ng-click, 'handleSave')]")
+
+    @property
+    def is_displayed(self):
+        return self.browser.wait_for_element(
+            self.name, exception=False) is not None
+
+
+class LCEEditView(BaseLoggedInView):
+    return_to_all = Text("//a[text()='Environments']")
+
+    @property
+    def is_displayed(self):
+        return self.browser.wait_for_element(
+            self.return_to_all, exception=False) is not None
+
+    @View.nested
+    class details(SatTab):
+        name = EditableEntry(name='Name')
+        label = ReadOnlyEntry(name='Label')
+        description = EditableEntry(name='Description')
