72f8474b82fc994603a116a8db28225bcca959c5
oshtaier <alexshtayer@yahoo.com>
2018-05-08 12:46:10 +0300

    Add Products entity(part_1)

---
+++ airgun/entities/product.py
@@ -0,0 +1,68 @@
+from navmazing import NavigateToSibling
+
+from airgun.entities.base import BaseEntity
+from airgun.navigation import NavigateStep, navigator
+from airgun.views.product import (
+    ProductCreateView,
+    ProductEditView,
+    ProductsTableView,
+)
+
+
+class ProductEntity(BaseEntity):
+
+    def create(self, values):
+        view = self.navigate_to(self, 'New')
+        view.fill(values)
+        view.submit.click()
+
+    def delete(self, entity_name):
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        view.actions.fill('Remove Product')
+        view.dialog.confirm()
+
+    def search(self, value):
+        view = self.navigate_to(self, 'All')
+        return view.search(value)
+
+    def read(self, entity_name):
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        return view.read()
+
+    def update(self, entity_name, values):
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        return view.fill(values)
+
+    def add_yum_repo(self, entity_name, repo_name):
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        view.yumrepo.repos.add(repo_name)
+
+
+@navigator.register(ProductEntity, 'All')
+class ShowAllProducts(NavigateStep):
+    VIEW = ProductsTableView
+
+    def step(self, *args, **kwargs):
+        self.view.menu.select('Content', 'Products')
+
+
+@navigator.register(ProductEntity, 'New')
+class AddNewProduct(NavigateStep):
+    VIEW = ProductCreateView
+
+    prerequisite = NavigateToSibling('All')
+
+    def step(self, *args, **kwargs):
+        self.parent.browser.click(self.parent.new)
+
+
+@navigator.register(ProductEntity, 'Edit')
+class EditContentView(NavigateStep):
+    VIEW = ProductEditView
+
+    def prerequisite(self, *args, **kwargs):
+        return self.navigate_to(self.obj, 'All')
+
+    def step(self, *args, **kwargs):
+        self.parent.search(kwargs.get('entity_name'))
+        self.parent.edit.click()
