dab8bb4d8abfa78fb4face65aaa0ca5e3817b4ba
Andrii Balakhtar <balakhtara@gmail.com>
2018-05-11 16:25:49 +0300

    Introduce Progress Bar widget and Content View Publish/Promote helpers

--- airgun/views/contentview.py
+++ airgun/views/contentview.py
@@ -1,9 +1,17 @@
-from widgetastic.widget import Checkbox, Text, TextInput, View
+from widgetastic.widget import (
+    Checkbox,
+    ParametrizedView,
+    Text,
+    TextInput,
+    View,
+)
 
 from airgun.views.common import (
     AddRemoveResourcesView,
     BaseLoggedInView,
+    LCESelectorGroup,
     SatTab,
+    SatTable,
     SatTabWithDropdown,
     SearchableViewMixin,
 )
@@ -12,7 +20,9 @@ from airgun.widgets import (
     ConfirmationDialog,
     EditableEntry,
     EditableEntryCheckbox,
+    PublishPromoteProgressBar,
     ReadOnlyEntry,
+    Search,
 )
 
 
@@ -56,7 +66,7 @@ class ContentViewEditView(BaseLoggedInView):
             self.return_to_all, exception=False) is not None
 
     @View.nested
-    class Details(SatTab):
+    class details(SatTab):
         name = EditableEntry(name='Name')
         label = ReadOnlyEntry(name='Label')
         description = EditableEntry(name='Description')
@@ -64,8 +74,64 @@ class ContentViewEditView(BaseLoggedInView):
         force_puppet = EditableEntryCheckbox(name='Force Puppet')
 
     @View.nested
+    class versions(SatTab):
+        searchbox = Search()
+        resources = SatTable(
+            locator='//table',
+            column_widgets={
+                'Version': Text('.//a'),
+                'Status': PublishPromoteProgressBar(),
+                'Actions': ActionsDropdown(
+                    './div[contains(@class, "btn-group")]')
+            },
+        )
+
+        def search(self, version_name):
+            """Searches for content view version.
+
+            Searchbox can't search by version name, only by id, that's why in
+            case version name was passed, it's transformed into recognizable
+            value before filling, for example::
+
+                'Version 1.0' -> 'version = 1'
+            """
+            search_phrase = version_name
+            if version_name.startswith('V') and '.' in version_name:
+                search_phrase = 'version = {}'.format(
+                    version_name.split()[1].split('.')[0])
+            self.searchbox.search(search_phrase)
+            return self.resources.read()
+
+    @View.nested
     class yumrepo(SatTabWithDropdown):
         TAB_NAME = 'Yum Content'
         SUB_ITEM = 'Repositories'
 
         repos = View.nested(AddRemoveResourcesView)
+
+
+class ContentViewVersionPublishView(BaseLoggedInView):
+    version = Text('//div[@label="Version"]/div/span')
+    description = TextInput(id='description')
+    force_metadata_regeneration = Checkbox(id='forceMetadataRegeneration')
+    save = Text('//button[contains(@ng-click, "handleSave()")]')
+    cancel = Text('//button[contains(@ng-click, "handleCancel()")]')
+
+    @property
+    def is_displayed(self):
+        return self.browser.wait_for_element(
+            self.save, exception=False) is not None
+
+
+class ContentViewVersionPromoteView(BaseLoggedInView):
+    lce = ParametrizedView.nested(LCESelectorGroup)
+    description = TextInput(id='description')
+    force_metadata_regeneration = Checkbox(id='forceMetadataRegeneration')
+    promote = Text('//button[contains(@ng-click, "verifySelection()")]')
+    cancel = Text(
+        '//a[contains(@class, "btn")][@ui-sref="content-view.versions"]')
+
+    @property
+    def is_displayed(self):
+        return self.browser.wait_for_element(
+            self.save, exception=False) is not None
