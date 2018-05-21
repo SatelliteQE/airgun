a7dfc13779fdb892d3c0256f255d6718d6238321
Tomas Strych <tstrych@redhat.com>
2018-05-21 18:44:57 +0200

    update option added, happened during the test writing

--- airgun/entities/partitiontable.py
+++ airgun/entities/partitiontable.py
@@ -23,6 +23,11 @@ class PartitionTableEntity(BaseEntity):
         view = self.navigate_to(self, 'All')
         return view.search(value)
 
+    def update(self, values, entity_name):
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        view.fill(values)
+        view.submit.click()
+
     def clone(self, values, entity_name):
         view = self.navigate_to(self, 'Clone', entity_name=entity_name)
         view.fill(values)
