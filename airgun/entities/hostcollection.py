0dc9091227377737b8be1fe4119ccf6dd8f5a273
ldjebran <ldjebran@gmail.com>
2018-08-21 15:52:21 +0300

    Implement Host Collection Content Management Actions

--- airgun/entities/hostcollection.py
+++ airgun/entities/hostcollection.py
@@ -6,6 +6,10 @@ from airgun.views.hostcollection import (
     HostCollectionCreateView,
     HostCollectionEditView,
     HostCollectionsView,
+    HostCollectionChangeAssignedContentView,
+    HostCollectionInstallErrataView,
+    HostCollectionManagePackagesView,
+    HostCollectionActionTaskDetailsView,
 )
 
 
@@ -35,16 +39,59 @@ class HostCollectionEntity(BaseEntity):
 
     def update(self, entity_name, values):
         view = self.navigate_to(self, 'Edit', entity_name=entity_name)
-        filled_values = view.fill(values)
-        view.flash.assert_no_error()
-        view.flash.dismiss()
-        return filled_values
+        return view.details.fill(values)
 
     def associate_host(self, entity_name, host_name):
         view = self.navigate_to(self, 'Edit', entity_name=entity_name)
         view.hosts.resources.add(host_name)
         view.flash.assert_no_error()
         view.flash.dismiss()
+
+    def manage_packages(
+            self, entity_name, content_type='Package', packages=None,
+            action='install', action_via='via Katello Agent'):
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        view.details.manage_packages.click()
+        view = HostCollectionManagePackagesView(view.browser)
+        if content_type is not None:
+            view.content_type.fill(content_type)
+        if packages is not None:
+            view.packages.fill(packages)
+        view.apply_action(action, action_via=action_via)
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
+    def install_errata(self, entity_name, errata_id,
+                       install_via='via Katello agent'):
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        view.details.install_errata.click()
+        view = HostCollectionInstallErrataView(view.browser)
+        view.search.fill(errata_id)
+        view.table.row(Id=errata_id)[0].widget.fill(True)
+        if install_via:
+            view.install.select(install_via)
+        else:
+            view.install.click()
+            view.dialog.confirm()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+        task_view = HostCollectionActionTaskDetailsView(view.browser)
+        task_view.progressbar.wait_for_result()
+        return task_view.read()
+
+    def change_assigned_content(self, entity_name, lce, content_view):
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        view.details.change_assigned_content.click()
+        view = HostCollectionChangeAssignedContentView(view.browser)
+        view.lce.fill({lce: True})
+        view.content_view.fill(content_view)
+        view.assign.click()
+        view.dialog.confirm()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+        task_view = HostCollectionActionTaskDetailsView(view.browser)
+        task_view.progressbar.wait_for_result()
+        return task_view.read()
 
 
 @navigator.register(HostCollectionEntity, 'All')
--- airgun/views/hostcollection.py
+++ airgun/views/hostcollection.py
@@ -1,13 +1,29 @@
-from widgetastic.widget import Checkbox, Text, TextInput, View
+from widgetastic.widget import (
+    Checkbox,
+    Text,
+    TextInput,
+    ParametrizedView,
+    Select,
+    View,
+)
 from widgetastic_patternfly import BreadCrumb
 
 from airgun.views.common import (
     AddRemoveResourcesView,
     BaseLoggedInView,
+    LCESelectorGroup,
     SatTab,
     SearchableViewMixin,
+    TaskDetailsView,
 )
-from airgun.widgets import EditableEntry, SatTable
+from airgun.widgets import (
+    ActionsDropdown,
+    ConfirmationDialog,
+    EditableEntry,
+    EditableLimitEntry,
+    RadioGroup,
+    SatTable,
+)
 
 
 class HostCollectionsView(BaseLoggedInView, SearchableViewMixin):
@@ -57,9 +73,236 @@ class HostCollectionEditView(BaseLoggedInView):
     class details(SatTab):
         name = EditableEntry(name='Name')
         description = EditableEntry(name='Description')
+        content_host_limit = EditableLimitEntry(name='Content Host Limit')
+        # Package Installation, Removal, and Update
+        manage_packages = Text(".//li[@bst-feature-flag='remote_actions']"
+                               "/a[@ng-click='openPackagesModal()']")
+        # Errata Installation
+        install_errata = Text(".//li[@bst-feature-flag='remote_actions']"
+                              "/a[@ng-click='openErrataModal()']")
+        # Change assigned Lifecycle Environment or Content View
+        change_assigned_content = Text(
+            ".//li[@bst-feature-flag='lifecycle_environments']"
+            "/a[@ng-click='openEnvironmentModal()']"
+        )
+
+        @View.nested
+        class content_hosts(View):
+            ROOT = ("//dt[normalize-space(.)='Content Hosts']"
+                    "/following-sibling::dd[1]")
+            add_hosts = Text(
+                "./a[contains(@ui-sref, 'host-collection.hosts.add')]")
+            list_hosts = Text(
+                "./a[contains(@ui-sref, 'host-collection.hosts.list')]")
+
+            def read(self):
+                if self.add_hosts.is_displayed:
+                    return self.add_hosts.read()
+                else:
+                    return self.list_hosts.read()
+
+            def click(self):
+                if self.add_hosts.is_displayed:
+                    # This click should open hosts>resources>AddTab
+                    return self.add_hosts.click()
+                else:
+                    # This click should open hosts>resources>ListRemoveTab
+                    return self.list_hosts.click()
 
     @View.nested
     class hosts(SatTab):
         TAB_NAME = 'Hosts'
 
         resources = View.nested(AddRemoveResourcesView)
+
+
+class HostCollectionPackageContentRadioGroup(RadioGroup):
+    """Handle an HTML non normalized Radio group according to the current
+    architecture.
+    Note: This is a temporary solution, a fix will be issued upstream,
+        when the fix will be available downstream we should replace the
+        implementation with RadioGroup.
+    Example html representation::
+
+        <div >
+            <input type="radio" id="package" ...>
+            <label>Package</label>
+            <input type="radio" id="package_group" ...>
+            <label>Package Group</label>
+      """
+    # a mapping between button name and the id, see the implementation in
+    # HostsAssignOrganization and HostsAssignLocation hereafter.
+    buttons_name_id_map = {
+        'Package': 'package',
+        'Package Group': 'package_group',
+    }
+
+    def get_input_by_name(self, name):
+        input_id = self.buttons_name_id_map.get(name)
+        if input_id:
+            return self.browser.wait_for_element(
+                ".//input[@id='{0}']".format(self.buttons_name_id_map[name])
+            )
+        return None
+
+    @property
+    def selected(self):
+        """Return the name of the button that is currently selected."""
+        for name in self.buttons_name_id_map.keys():
+            btn = self.get_input_by_name(name)
+            if btn and btn.get_attribute('checked') is not None:
+                return name
+        else:
+            raise ValueError(
+                "Whether no radio button is selected or proper attribute "
+                "should be added to framework"
+            )
+
+    def select(self, name):
+        """Select specific radio button in the group"""
+        if self.selected != name:
+            btn = self.get_input_by_name(name)
+            if btn:
+                btn.click()
+                return True
+        return False
+
+    def read(self):
+        """Wrap method according to architecture"""
+        return self.selected
+
+    def fill(self, name):
+        """Wrap method according to architecture"""
+        return self.select(name)
+
+
+class HostCollectionManagePackagesView(BaseLoggedInView):
+    title = Text("//h4[contains(., 'Update Packages')]")
+    update_all = ActionsDropdown(
+        "//button[contains(@class, 'btn')][contains(@ng-click, 'update all')]"
+        "/ancestor::span[contains(@class, 'input-group')]"
+    )
+    content_type = HostCollectionPackageContentRadioGroup(
+        "//div[@name='systemContentForm']/div")
+
+    packages = TextInput(
+        locator=("//input[@type='text' and "
+                 "contains(@ng-model, 'content.content')]")
+    )
+    install = ActionsDropdown(
+        "//button[contains(@class, 'btn')][contains(@ng-click, 'install')]"
+        "/ancestor::span[contains(@class, 'input-group')]"
+    )
+    update = ActionsDropdown(
+        "//button[contains(@class, 'btn') and contains(@ng-click, 'update')"
+        " and not(contains(@ng-click, 'update all'))]"
+        "/ancestor::span[contains(@class, 'input-group')]"
+    )
+    remove = ActionsDropdown(
+        "//button[contains(@class, 'btn')][contains(@ng-click, 'remove')]"
+        "/ancestor::span[contains(@class, 'input-group')]"
+    )
+    done = Text("//button[@ng-click='ok()']")
+
+    @property
+    def is_displayed(self):
+        """The view is displayed when it's title exists"""
+        return self.browser.wait_for_element(
+            self.title, exception=False) is not None
+
+    @View.nested
+    class dialog(ConfirmationDialog):
+        ROOT = ".//div[@class='inline-confirmation']"
+        confirm_dialog = Text(
+            locator=("//div[@ng-show='content.confirm']"
+                     "//button[@type='submit']")
+        )
+        cancel_dialog = Text(
+            locator=("//div[@ng-show='content.confirm']"
+                     "//button[@type='button']"
+                     "[@ng-click='content.confirm = false']")
+        )
+
+    def get_action_button(self, name):
+        """Return an action button by it's name"""
+        action_button = getattr(self, name)
+        if not isinstance(action_button, ActionsDropdown):
+            raise ValueError(
+                'Action with name: "{0}" does not exists'.format(name))
+        return action_button
+
+    def apply_action(self, name, action_via='via Katello Agent'):
+        """Apply an action by name using action via if indicated"""
+        action_button = self.get_action_button(name)
+        if action_via:
+            action_button.select(action_via)
+        else:
+            action_button.button.click()
+            self.dialog.confirm()
+
+
+class HostCollectionInstallErrataView(BaseLoggedInView):
+    title = Text("//h4[contains(., 'Content Host Errata Management')]")
+
+    search = TextInput(
+        locator=".//input[@type='text'][@ng-model='errataFilter']")
+    refresh = Text(locator=".//button[@ng-click='fetchErrata()']")
+    install = ActionsDropdown(
+            "//button[contains(@class, 'btn')]"
+            "[contains(@ng-click, 'showConfirm')]"
+            "/ancestor::span[contains(@class, 'btn-group')]"
+    )
+    table = SatTable(
+        ".//table",
+        column_widgets={
+            0: Checkbox(
+                locator=".//input[@ng-model='erratum.selected']"),
+            'Id': Text(locator="./a[@ng-click='transitionToErrata(erratum)']"),
+        }
+    )
+    dialog = ConfirmationDialog()
+
+    @property
+    def is_displayed(self):
+        """The view is displayed when it's title exists"""
+        return self.browser.wait_for_element(
+            self.title, exception=False) is not None
+
+
+class HostCollectionChangeAssignedContentView(BaseLoggedInView):
+    title = Text("//h4[contains(., 'Content Host Bulk Content')]")
+
+    lce = ParametrizedView.nested(LCESelectorGroup)
+    content_view = Select(
+        locator=".//select[@ng-model='selected.contentView']")
+    assign = Text(
+        locator=".//form//button[contains(@ng-click, 'showConfirm')]")
+
+    @View.nested
+    class dialog(ConfirmationDialog):
+        confirm_dialog = Text(
+            ".//div[@ng-show='showConfirm']"
+            "//button[contains(@ng-click, 'performAction')]"
+        )
+        cancel_dialog = Text(
+            ".//div[@ng-show='showConfirm']"
+            "//button[contains(@ng-click, 'showConfirm')"
+            " and not(contains(@ng-click, 'performAction'))]"
+        )
+
+    @property
+    def is_displayed(self):
+        """The view is displayed when it's title exists"""
+        return self.browser.wait_for_element(
+            self.title, exception=False) is not None
+
+
+class HostCollectionActionTaskDetailsView(TaskDetailsView):
+    title = Text("//h4[contains(., 'Task Details')]")
+    breadcrumb = None
+
+    @property
+    def is_displayed(self):
+        """The view is displayed when it's title exists"""
+        return self.browser.wait_for_element(
+            self.title, exception=False) is not None
