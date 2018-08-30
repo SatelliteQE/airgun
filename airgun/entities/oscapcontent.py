15be7c787a8f05866d2796544a13970778e33166
fjkdl <tomas.strych2@gmail.com>
2018-08-30 12:46:39 +0200

    OpenSCAP content entity and view (#166)
    * changes applied and rebased
    
    * file_name added
    
    * specified xpath of element

---
+++ airgun/entities/oscapcontent.py
@@ -0,0 +1,103 @@
+from navmazing import NavigateToSibling
+
+from airgun.entities.base import BaseEntity
+from airgun.navigation import NavigateStep, navigator
+from airgun.views.oscapcontent import (
+    SCAPContentCreateView,
+    SCAPContentEditView,
+    SCAPContentsView,
+)
+
+
+class OSCAPContentEntity(BaseEntity):
+
+    def create(self, values):
+        """Create new SCAP Conent
+
+        :param values: Parameters to be assigned to new SCAP content,
+            manadatory values are Title and Scap file.
+        """
+        view = self.navigate_to(self, 'New')
+        view.fill(values)
+        view.submit.click()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
+    def delete(self, entity_name):
+        """Delete corresponding SCAP Content
+
+        :param entity_name: title of the corresponding SCAP Content
+        """
+        view = self.navigate_to(self, 'All')
+        view.search(entity_name)
+        view.table.row(title=entity_name)['Actions'].widget.fill('Delete')
+        self.browser.handle_alert()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
+    def search(self, title):
+        """Search for SCAP Content
+
+        :param title: title of the corresponding SCAP Content
+        :return: result of the SCAP Content search
+        """
+        view = self.navigate_to(self, 'All')
+        return view.search(title)
+
+    def read(self, entity_name):
+        """Reads the content of corresponding SCAP Content
+
+        :param entity_name: specify corresponding SCAP Content
+        :return: dict representing tabs, with nested dicts representing fields
+        """
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        return view.read()
+
+    def update(self, entity_name, values):
+        """Updates instance of SCAP Content with new values
+
+        :param entity_name: specify corresponding SCAP Content
+        :param values: updates individual parameters of SCAP Content instance
+        """
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        view.fill(values)
+        view.submit.click()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
+
+@navigator.register(OSCAPContentEntity, 'All')
+class ShowAllSCAPContents(NavigateStep):
+    """Navigate to All SCAP Contents screen."""
+    VIEW = SCAPContentsView
+
+    def step(self, *args, **kwargs):
+        self.view.menu.select('Hosts', 'SCAP contents')
+
+
+@navigator.register(OSCAPContentEntity, 'New')
+class UploadNewSCAPContent(NavigateStep):
+    """Navigate to upload new SCAP Content page."""
+    VIEW = SCAPContentCreateView
+
+    def am_i_here(self, *args, **kwargs):
+        return self.view.is_displayed and self.view.title == ''
+
+    prerequisite = NavigateToSibling('All')
+
+    def step(self, *args, **kwargs):
+        self.parent.browser.click(self.parent.new)
+
+
+@navigator.register(OSCAPContentEntity, 'Edit')
+class EditSCAPContent(NavigateStep):
+    """Navigate to edit existing SCAP Content page."""
+    VIEW = SCAPContentEditView
+
+    def prerequisite(self, *args, **kwargs):
+        return self.navigate_to(self.obj, 'All')
+
+    def step(self, *args, **kwargs):
+        entity_name = kwargs.get('entity_name')
+        self.parent.search(entity_name)
+        self.parent.table.row(title=entity_name)['Actions'].widget.fill('Edit')
---
+++ airgun/views/oscapcontent.py
@@ -0,0 +1,73 @@
+from widgetastic.widget import (
+    FileInput,
+    Text,
+    TextInput,
+    View,
+)
+from widgetastic_patternfly import BreadCrumb
+
+from airgun.views.common import (
+    BaseLoggedInView,
+    SatTab,
+    SearchableViewMixin,
+)
+from airgun.widgets import (
+    ActionsDropdown,
+    MultiSelect,
+    SatTable,
+)
+
+
+class SCAPContentsView(BaseLoggedInView, SearchableViewMixin):
+    title = Text("//h1[text()='SCAP Contents']")
+    new = Text("//a[contains(@href, 'scap_contents/new')]")
+    table = SatTable(
+        './/table',
+        column_widgets={
+            'Title': Text('./a'),
+            'Actions': ActionsDropdown("./div[contains(@class, 'btn-group')]"),
+        }
+    )
+
+    @property
+    def is_displayed(self):
+        return self.browser.wait_for_element(
+            self.title, exception=False) is not None
+
+
+class SCAPContentCreateView(BaseLoggedInView):
+    create_form = Text("//form[@id='new_scap_content']")
+    submit = Text('//input[@name="commit"]')
+    cancel = Text('//a[text()="Cancel"]')
+
+    @View.nested
+    class file_upload(SatTab):
+        TAB_NAME = 'File Upload'
+        title = TextInput(id='scap_content_title')
+        scap_file = FileInput(id='scap_content_scap_file')
+
+    @View.nested
+    class locations(SatTab):
+        resources = MultiSelect(id='ms-scap_content_location_ids')
+
+    @View.nested
+    class organizations(SatTab):
+        resources = MultiSelect(id='ms-scap_content_organization_ids')
+
+    def is_displayed(self):
+        return self.browser.wait_for_element(
+            self.create_form, exception=False) is not None
+
+
+class SCAPContentEditView(SCAPContentCreateView):
+    scap_file_name = Text('//div[@class="col-md-4"]/b')
+    breadcrumb = BreadCrumb()
+
+    @property
+    def is_displayed(self):
+        breadcrumb_loaded = self.browser.wait_for_element(
+            self.breadcrumb, exception=False)
+        return (
+                breadcrumb_loaded
+                and self.breadcrumb.locations[0] == 'Scap Contents'
+        )
