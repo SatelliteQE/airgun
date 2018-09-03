56ee34b05519dd41238097ad3cacb05f59f44a81
Dmitrii Misharov <dmisharo@redhat.com>
2018-09-03 15:32:29 +0200

    added docstrings

--- airgun/entities/rhai/manage.py
+++ airgun/entities/rhai/manage.py
@@ -11,12 +11,15 @@ class ManageEntity(BaseEntity):
             view.save.click()
 
     def enable_service(self):
+        """Enable the service."""
         self._toggle_service(True)
 
     def disable_service(self):
+        """Disable the service."""
         self._toggle_service(False)
 
     def read(self):
+        """Read the content of the view."""
         view = self.navigate_to(self, "Details")
         view.check_connection.click()
         return view.read()
@@ -24,6 +27,7 @@ class ManageEntity(BaseEntity):
 
 @navigator.register(ManageEntity, "Details")
 class ManageDetails(NavigateStep):
+    """Navigate to Red Hat Access Insights Manage screen."""
     VIEW = ManageDetailsView
 
     def step(self, *args, **kwargs):
