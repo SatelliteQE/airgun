dcf2c952110572d4f3d227f28e4fd73c456894df
Tomas Strych <tstrych@redhat.com>
2018-09-04 18:21:31 +0200

    Oscap policy entity

---
+++ airgun/entities/oscappolicy.py
@@ -0,0 +1,122 @@
+from navmazing import NavigateToSibling
+
+from airgun.entities.base import BaseEntity
+from airgun.navigation import NavigateStep, navigator
+from airgun.views.oscappolicy import (
+    SCAPPoliciesView,
+    SCAPPolicyCreateView,
+    SCAPPolicyDetailsView,
+    SCAPPolicyEditView,
+)
+
+
+class OSCAPPolicyEntity(BaseEntity):
+
+    def create(self, values):
+        """Creates new OSCAP Policy
+
+        :param values: Parameters to be assigned to new SCAP policy,
+            mandatory values are Name, Scap Content and Period,
+            with chosen date when policy is going to repeat
+        """
+        view = self.navigate_to(self, 'New')
+        view.fill(values)
+        view.host_group.submit.click()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
+    def delete(self, entity_name):
+        """Delete corresponding SCAP Policy
+
+        :param entity_name: name of the corresponding SCAP Policy
+        """
+        view = self.navigate_to(self, 'All')
+        view.search(entity_name)
+        view.table.row(name=entity_name)['Actions'].widget.fill('Delete')
+        self.browser.handle_alert()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
+    def search(self, entity_name):
+        """Search for SCAP Policy
+
+        :param entity_name: name of the corresponding SCAP Policy
+        :return: result of the SCAP Policy search
+        """
+        view = self.navigate_to(self, 'All')
+        return view.search(entity_name)
+
+    def details(self, entity_name):
+        """Read the content from corresponding SCAP Policy dashboard,
+            clicking on the Name of SCAP Policy shows the dashboard
+
+        :param entity_name:
+        :return: dictionary with values from SCAP Policy Details View
+        """
+        view = self.navigate_to(self, 'All')
+        view.search(entity_name)
+        view.table.row(name=entity_name)['Name'].widget.click()
+        view = self.navigate_to(self, 'Details', entity_name=entity_name)
+        return view.read()
+
+    def read(self, entity_name):
+        """Reads the values of corresponding SCAP Policy - edit menu
+
+        :param entity_name: specifies corresponding SCAP Policy
+        :return: dict representing tabs, with nested dicts representing fields
+        """
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        return view.read()
+
+    def update(self, entity_name, values):
+        """Updates instance of SCAP Policy with new values
+
+        :param entity_name: specifies corresponding SCAP Policy
+        :param values: updates individual parameters of corresponding
+            SCAP Policy
+        """
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        view.fill(values)
+        view.submit.click()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
+
+@navigator.register(OSCAPPolicyEntity, 'All')
+class ShowAllSCAPPolicies(NavigateStep):
+    """Navigate to All SCAP Policies screen."""
+    VIEW = SCAPPoliciesView
+
+    def step(self, *args, **kwargs):
+        self.view.menu.select('Hosts', 'Policies')
+
+
+@navigator.register(OSCAPPolicyEntity, 'New')
+class NewSCAPPolicy(NavigateStep):
+    """Navigate to upload new SCAP Policies page."""
+    VIEW = SCAPPolicyCreateView
+
+    prerequisite = NavigateToSibling('All')
+
+    def step(self, *args, **kwargs):
+        self.parent.new.click()
+
+
+@navigator.register(OSCAPPolicyEntity, 'Edit')
+class EditSCAPPolicy(NavigateStep):
+    """Navigate to edit existing SCAP Policy page."""
+    VIEW = SCAPPolicyEditView
+
+    def prerequisite(self, *args, **kwargs):
+        return self.navigate_to(self.obj, 'All')
+
+    def step(self, *args, **kwargs):
+        entity_name = kwargs.get('entity_name')
+        self.parent.search(entity_name)
+        self.parent.table.row(name=entity_name)['Actions'].widget.fill('Edit')
+
+
+@navigator.register(OSCAPPolicyEntity, 'Details')
+class DetailsSCAPPolicy(NavigateStep):
+    """To get data from SCAPPolicyDetail view"""
+    VIEW = SCAPPolicyDetailsView
---
+++ airgun/views/oscappolicy.py
@@ -0,0 +1,191 @@
+from widgetastic.widget import (
+    ConditionalSwitchableView,
+    Text,
+    TextInput,
+    View,
+)
+from widgetastic_patternfly import BreadCrumb
+
+from airgun.views.common import (
+    BaseLoggedInView,
+    SatTab,
+    SearchableViewMixin,
+)
+
+from airgun.widgets import (
+    ActionsDropdown,
+    FilteredDropdown,
+    MultiSelect,
+    SatTable,
+)
+
+
+class SCAPPoliciesView(BaseLoggedInView, SearchableViewMixin):
+    title = Text("//h1[text()='Compliance Policies']")
+    new = Text("//a[contains(@href, '/compliance/policies/new')]")
+    table = SatTable(
+        './/table',
+        column_widgets={
+            'Name': Text('./a'),
+            'Actions': ActionsDropdown("./div[contains(@class, 'btn-group')]"),
+        }
+    )
+
+    @property
+    def is_displayed(self):
+        return self.browser.wait_for_element(
+            self.title, exception=False) is not None
+
+
+class SCAPPolicyCreateView(BaseLoggedInView):
+    breadcrumb = BreadCrumb()
+
+    @property
+    def is_displayed(self):
+        breadcrumb_loaded = self.browser.wait_for_element(
+            self.breadcrumb, exception=False)
+        return (
+            breadcrumb_loaded
+            and self.breadcrumb.locations[0] == 'Policies'
+            and self.breadcrumb.read() == 'New Compliance Policy'
+        )
+
+    @View.nested
+    class create_policy(BaseLoggedInView):
+        TAB_NAME = 'Create policy'
+        next_step = Text("//input[contains(@value, 'Next')]")
+        name = TextInput(id='policy_name')
+        description = TextInput(id='policy_description')
+
+        def after_fill(self, was_change):
+            self.next_step.click()
+
+    @View.nested
+    class scap_content(BaseLoggedInView):
+        TAB_NAME = 'SCAP Content'
+        next_step = Text("//input[contains(@value, 'Next')]")
+        scap_content_resource = FilteredDropdown(
+            id='s2id_policy_scap_content_id')
+        xccdf_profile = FilteredDropdown(
+            id='s2id_policy_scap_content_profile_id')
+        tailoring_file = FilteredDropdown(id='s2id_policy_tailoring_file_id')
+        xccdf_profile_tailoring_file = FilteredDropdown(
+            id='s2id_policy_tailoring_file_profile_id')
+
+        def after_fill(self, was_change):
+            self.next_step.click()
+
+    @View.nested
+    class schedule(BaseLoggedInView):
+        next_step = Text("//input[contains(@value, 'Next')]")
+        period = FilteredDropdown(id='s2id_policy_period')
+        period_selection = ConditionalSwitchableView(reference='period')
+
+        @period_selection.register('Weekly')
+        class WeeklyPeriodForm(View):
+            weekday = FilteredDropdown(id='s2id_policy_weekday')
+
+        @period_selection.register('Monthly')
+        class MonthlyPeriodForm(View):
+            day_of_month = FilteredDropdown(id='s2id_policy_day_of_month')
+
+        @period_selection.register('Custom')
+        class CustomPeriodForm(View):
+            cron_line = TextInput(id='policy_cron_line')
+
+        def after_fill(self, was_change):
+            self.next_step.click()
+
+    @View.nested
+    class locations(BaseLoggedInView):
+        next_step = Text("//input[contains(@value, 'Next')]")
+        resources = MultiSelect(id='ms-policy_location_ids')
+
+        def after_fill(self, was_change):
+            self.next_step.click()
+
+    @View.nested
+    class organizations(BaseLoggedInView):
+        next_step = Text("//input[contains(@value, 'Next')]")
+        resources = MultiSelect(id='ms-policy_organization_ids')
+
+        def after_fill(self, was_change):
+            self.next_step.click()
+
+    @View.nested
+    class host_group(BaseLoggedInView):
+        TAB_NAME = 'Host Groups'
+        submit = Text('//input[@name="commit"]')
+        resources = MultiSelect(id='ms-policy_hostgroup_ids')
+
+
+class SCAPPolicyEditView(BaseLoggedInView):
+    submit = Text('//input[@name="commit"]')
+    cancel = Text("//a[text()='Cancel']")
+    breadcrumb = BreadCrumb()
+
+    @property
+    def is_displayed(self):
+        breadcrumb_loaded = self.browser.wait_for_element(
+            self.breadcrumb, exception=False)
+        return (
+            breadcrumb_loaded
+            and self.breadcrumb.locations[0] == 'Policies'
+            and self.breadcrumb.read() != 'New Compliance Policy'
+        )
+
+    @View.nested
+    class general(SatTab):
+        name = TextInput(id='policy_name')
+        description = Text('//textarea[@id="policy_description"]')
+
+    @View.nested
+    class scap_content(SatTab):
+        TAB_NAME = 'SCAP Content'
+        scap_content = FilteredDropdown(id='s2id_policy_scap_content_id')
+        xccdf_profile = FilteredDropdown(
+            id='s2id_policy_scap_content_profile_id')
+        tailoring_file = FilteredDropdown(id='s2id_policy_tailoring_file_id')
+        xccdf_profile_tailoring_file = FilteredDropdown(
+            id='s2id_policy_tailoring_file_profile_id')
+
+    @View.nested
+    class schedule(SatTab):
+        period = FilteredDropdown(id='s2id_policy_period')
+        period_selection = ConditionalSwitchableView(reference='period')
+
+        @period_selection.register('Weekly')
+        class WeeklyPeriodForm(View):
+            weekday = FilteredDropdown(id='s2id_policy_weekday')
+
+        @period_selection.register('Monthly')
+        class MonthlyPeriodForm(View):
+            day_of_month = FilteredDropdown(id='s2id_policy_day_of_month')
+
+        @period_selection.register('Custom')
+        class CustomPeriodForm(View):
+            cron_line = TextInput(id='policy_cron_line')
+
+    @View.nested
+    class locations(SatTab):
+        resources = MultiSelect(id='ms-policy_location_ids')
+
+    @View.nested
+    class organizations(SatTab):
+        resources = MultiSelect(id='ms-policy_organization_ids')
+
+    @View.nested
+    class host_group(SatTab):
+        TAB_NAME = 'Host Groups'
+        resources = MultiSelect(id='ms-policy_hostgroup_ids')
+
+
+class SCAPPolicyDetailsView(BaseLoggedInView):
+    title = Text('h1[text()[contains(., "Compliance policy")]]')
+    total_hosts = Text('//a[text()[contains(.,"Total hosts")]]')
+    hosts_percentage = Text('//div[@class="percent"]')
+
+    @property
+    def is_displayed(self):
+        return self.browser.wait_for_element(
+            self.title, exception=False) is not None
