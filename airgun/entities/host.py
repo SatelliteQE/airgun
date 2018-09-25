f2931139f32a182e3308474b1d034f8501ba09f2
Djebran Lezzoum <ldjebran@gmail.com>
2018-09-25 10:26:16 +0300

    Fix host entity (#208)
    * Fix host entity
    
    * Fix host entity - 2

--- airgun/entities/host.py
+++ airgun/entities/host.py
@@ -1,3 +1,4 @@
+from wait_for import wait_for
 from navmazing import NavigateToSibling
 
 from airgun.entities.base import BaseEntity
@@ -22,8 +23,15 @@ class HostEntity(BaseEntity):
         view = self.navigate_to(self, 'New')
         view.fill(values)
         view.submit.click()
-        view.flash.assert_no_error()
-        view.flash.dismiss()
+        host_view = HostDetailsView(self.browser)
+        wait_for(
+            lambda: host_view.is_displayed is True,
+            timeout=60,
+            delay=1,
+            logger=host_view.logger
+        )
+        host_view.flash.assert_no_error()
+        host_view.flash.dismiss()
 
     def search(self, value):
         """Search for existing host entity"""
