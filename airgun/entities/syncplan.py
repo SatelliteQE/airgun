20bcead0b06a81a4fe5dda5206e73c163be11202
oshtaier <alexshtayer@yahoo.com>
2018-10-23 22:59:23 +0300

    Add more functionality to sync plans

--- airgun/entities/syncplan.py
+++ airgun/entities/syncplan.py
@@ -12,6 +12,7 @@ from airgun.views.syncplan import (
 class SyncPlanEntity(BaseEntity):
 
     def create(self, values):
+        """Create new sync plan"""
         view = self.navigate_to(self, 'New')
         view.fill(values)
         view.submit.click()
@@ -19,6 +20,7 @@ class SyncPlanEntity(BaseEntity):
         view.flash.dismiss()
 
     def delete(self, entity_name):
+        """Delete existing sync plan"""
         view = self.navigate_to(self, 'Edit', entity_name=entity_name)
         view.actions.fill('Remove')
         view.dialog.confirm()
@@ -26,23 +28,35 @@ class SyncPlanEntity(BaseEntity):
         view.flash.dismiss()
 
     def search(self, value):
+        """Search for sync plan"""
         view = self.navigate_to(self, 'All')
         return view.search(value)
 
     def read(self, entity_name):
+        """Read values for created sync plan"""
         view = self.navigate_to(self, 'Edit', entity_name=entity_name)
         return view.read()
 
     def update(self, entity_name, values):
+        """Update sync plan with necessary values"""
         view = self.navigate_to(self, 'Edit', entity_name=entity_name)
         filled_values = view.fill(values)
         view.flash.assert_no_error()
         view.flash.dismiss()
         return filled_values
 
+    def add_product(self, entity_name, products_list):
+        """Add product to sync plan"""
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        view.products.resources.add(products_list)
+        assert view.flash.is_displayed
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
 
 @navigator.register(SyncPlanEntity, 'All')
 class ShowAllSyncPlans(NavigateStep):
+    """Navigate to All Sync Plans screen."""
     VIEW = SyncPlansView
 
     def step(self, *args, **kwargs):
@@ -51,6 +65,7 @@ class ShowAllSyncPlans(NavigateStep):
 
 @navigator.register(SyncPlanEntity, 'New')
 class AddNewSyncPlan(NavigateStep):
+    """Navigate to New Sync Plan screen."""
     VIEW = SyncPlanCreateView
 
     prerequisite = NavigateToSibling('All')
@@ -61,6 +76,11 @@ class AddNewSyncPlan(NavigateStep):
 
 @navigator.register(SyncPlanEntity, 'Edit')
 class EditSyncPlan(NavigateStep):
+    """Navigate to Edit Sync Plan screen.
+
+    Args:
+        entity_name: name of sync plan
+    """
     VIEW = SyncPlanEditView
 
     def prerequisite(self, *args, **kwargs):
