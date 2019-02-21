fd9a1bf6cccfdebf56af9c3aef02e13894291c93
ldjebran <ldjebran@gmail.com>
2019-02-21 11:23:45 +0200

    Fixes Role and Filter entity - 2

--- airgun/exceptions.py
+++ airgun/exceptions.py
@@ -3,7 +3,3 @@
 
 class ReadOnlyWidgetError(Exception):
     """Raised mainly when trying to fill a read only widget"""
-
-
-class DisabledWidgetError(Exception):
-    """Raised when a widget is disabled, and not usable for some contexts scenarios"""
