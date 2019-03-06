6f78b62ed934f3958790986df1b0309929fb17df
Andrii Balakhtar <abalakh@users.noreply.github.com>
2019-03-06 18:08:20 +0200

    Fixes, new views and helpers for Container entity (#296)
    * Added some views and helpers for container entity
    
    * added missing docstring
    
    * added comment for bz workaround

--- airgun/entities/container.py
+++ airgun/entities/container.py
@@ -1,7 +1,7 @@
 from navmazing import NavigateToSibling
 from airgun.entities.base import BaseEntity
 from airgun.navigation import NavigateStep, navigator
-from airgun.views.container import ContainerCreateView, ContainerView
+from airgun.views.container import ContainerCreateView, ContainerDetailsView, ContainersView
 
 
 class ContainerEntity(BaseEntity):
@@ -19,11 +19,45 @@ class ContainerEntity(BaseEntity):
         view = self.navigate_to(self, 'All')
         return view.search(value)
 
+    def read(self, entity_name, widget_names=None):
+        """Read container details."""
+        view = self.navigate_to(self, 'Details', entity_name=entity_name)
+        return view.read(widget_names=widget_names)
+
+    def set_power(self, entity_name, power):
+        """Power on or power off specific container.
+
+        Args:
+            entity_name: name of container
+            power: power state to set container into. May be one of "On"/"Off"/True/False.
+        """
+        view = self.navigate_to(self, 'Details', entity_name=entity_name)
+        if power in (True, 'On'):
+            view.power_on.click()
+        elif power in (False, 'Off'):
+            view.power_off.click(handle_alert=True)
+        else:
+            raise ValueError(
+                'Wrong `power` value passed. Supported values are True/False/"On"/"Off"')
+        # workaround for BZ1683348
+        view.flash.assert_no_error(
+            ignore_messages=['Error - wrong number of arguments (given 1, expected 0)'])
+        view.flash.dismiss()
+
+    def delete(self, entity_name):
+        """Delete container."""
+        view = self.navigate_to(self, 'All')
+        view.search(entity_name)
+        view.table.row(name=entity_name.capitalize())['Action'].widget.fill('Delete')
+        self.browser.handle_alert()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
 
 @navigator.register(ContainerEntity, 'All')
 class ShowAllContainers(NavigateStep):
     """Navigate to All Containers screen"""
-    VIEW = ContainerView
+    VIEW = ContainersView
 
     def step(self, *args, **kwargs):
         self.view.menu.select('Containers', 'All Containers')
@@ -37,4 +71,26 @@ class AddNewContainer(NavigateStep):
     prerequisite = NavigateToSibling('All')
 
     def step(self, *args, **kwargs):
-        self.parent.browser.click(self.parent.new)
+        self.parent.new.click()
+
+
+@navigator.register(ContainerEntity, 'Details')
+class ContainerDetails(NavigateStep):
+    """Navigate to container details screen"""
+    VIEW = ContainerDetailsView
+
+    def prerequisite(self, *args, **kwargs):
+        return self.navigate_to(self.obj, 'All')
+
+    def am_i_here(self, *args, **kwargs):
+        entity_name = kwargs.get('entity_name')
+        return (
+            self.view.is_displayed
+            and self.view.breadcrumb.read() == '{} - /{}'.format(
+                entity_name.capitalize(), entity_name)
+        )
+
+    def step(self, *args, **kwargs):
+        entity_name = kwargs.get('entity_name')
+        self.parent.search(entity_name)
+        self.parent.table.row(name=entity_name.capitalize())['Name'].widget.click()
