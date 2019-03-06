507d680997496c00c03f927f9381af26b38ad33e
Oleksandr Shtaier <alexshtayer@yahoo.com>
2019-03-06 17:15:22 +0200

    Add hardware model entity and update dropdown widget (#297)
    * Add hardware model entity and update dropdown widget
    
    * Update airgun/widgets.py
    
    Co-Authored-By: oshtaier <alexshtayer@yahoo.com>

---
+++ airgun/entities/hardware_model.py
@@ -0,0 +1,85 @@
+from navmazing import NavigateToSibling
+
+from airgun.entities.base import BaseEntity
+from airgun.navigation import NavigateStep, navigator
+from airgun.views.hardware_model import (
+    HardwareModelCreateView,
+    HardwareModelEditView,
+    HardwareModelsView,
+)
+
+
+class HardwareModelEntity(BaseEntity):
+
+    def create(self, values):
+        """Create new hardware model"""
+        view = self.navigate_to(self, 'New')
+        view.fill(values)
+        view.submit.click()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
+    def search(self, value):
+        """Search for specific hardware model"""
+        view = self.navigate_to(self, 'All')
+        return view.search(value)
+
+    def read(self, entity_name, widget_names=None):
+        """Read values for existing hardware model"""
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        return view.read(widget_names=widget_names)
+
+    def update(self, entity_name, values):
+        """Update hardware model values"""
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        view.fill(values)
+        view.submit.click()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
+    def delete(self, entity_name):
+        """Delete hardware model"""
+        view = self.navigate_to(self, 'All')
+        view.search(entity_name)
+        view.table.row(name=entity_name)['Actions'].widget.click(
+            handle_alert=True)
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
+
+@navigator.register(HardwareModelEntity, 'All')
+class ShowAllHardwareModels(NavigateStep):
+    """Navigate to All Hardware Model screen."""
+    VIEW = HardwareModelsView
+
+    def step(self, *args, **kwargs):
+        self.view.menu.select('Hosts', 'Hardware Models')
+
+
+@navigator.register(HardwareModelEntity, 'New')
+class AddNewHardwareModel(NavigateStep):
+    """Navigate to Create new Hardware Model screen."""
+    VIEW = HardwareModelCreateView
+
+    prerequisite = NavigateToSibling('All')
+
+    def step(self, *args, **kwargs):
+        self.parent.new.click()
+
+
+@navigator.register(HardwareModelEntity, 'Edit')
+class EditHardwareModel(NavigateStep):
+    """Navigate to Edit Hardware Model screen.
+
+         Args:
+            entity_name: name of hardware model
+    """
+    VIEW = HardwareModelEditView
+
+    def prerequisite(self, *args, **kwargs):
+        return self.navigate_to(self.obj, 'All')
+
+    def step(self, *args, **kwargs):
+        entity_name = kwargs.get('entity_name')
+        self.parent.search(entity_name)
+        self.parent.table.row(name=entity_name)['Name'].widget.click()
