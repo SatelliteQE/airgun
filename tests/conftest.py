06c67f6d3a24d34c6af8d1c37837d12d7df0ad91
Mirosław Zalewski <mzalewsk@redhat.com>
2019-07-03 09:10:21 +0200

    Remove TODO and FIXME comments (closes #10) (#353)
    Source code comments are not the best project management tool. Plus we
    need single source of truth, not ideas scattered across multiple places.
    
    GitHub issues with references to these comments were created instead.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,6 @@ from airgun import settings
 pytest_plugins = ["airgun.fixtures"]
 
 
-# todo: make sure this hook is needed - for unit tests configuring may be
-# redundant
 def pytest_collection_modifyitems():
     """ called after collection has been performed, may filter or re-order
     the items in-place.
