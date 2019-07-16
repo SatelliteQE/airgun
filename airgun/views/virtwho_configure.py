963eb4682d1c7252e638c16debc6aee199be5264
Eko <hsun@redhat.com>
2019-07-16 13:51:38 +0800

    Get virtwho configure status by icon color

--- airgun/views/virtwho_configure.py
+++ airgun/views/virtwho_configure.py
@@ -2,11 +2,13 @@ from widgetastic_patternfly import BreadCrumb
 
 from widgetastic.widget import (
     Checkbox,
+    GenericLocatorWidget,
     ConditionalSwitchableView,
     Table,
     Text,
     TextInput,
     View,
+    Widget,
 )
 from airgun.views.common import (
     BaseLoggedInView,
@@ -19,6 +21,45 @@ from airgun.widgets import (
 )
 
 
+class StatusIcon(GenericLocatorWidget):
+    """The status for virtwho configure can be: green, gray.
+
+    green: The virt-who report has not arrived within the interval,
+         which indicates there was no change on hypervisor
+    gray: The configuration was not deployed yet or the virt-who was
+        unable to report the status
+    """
+
+    def __init__(self, parent, locator=None, logger=None):
+        """Provide default locator value if it wasn't passed"""
+        if not locator:
+            locator = ".//span[contains(@class, 'virt-who-config-report-status')]"
+        Widget.__init__(self, parent, logger=logger)
+        self.locator = locator
+
+    @property
+    def color(self):
+        """Returns string representing icon color: 'gray', 'green' or'unknown'.
+        rgba is created for chrome.
+        rbg is created for firefox.
+        """
+        colors = {
+            'rgba(54, 54, 54, 1)': 'gray',
+            'rgba(63, 156, 53, 1)': 'green',
+            'rgb(63, 156, 53)': 'green',
+            'rgb(54, 54, 54)': 'gray',
+        }
+        print(self.browser.element(self, parent=self.parent).value_of_css_property('color'))
+        return colors.get(
+            self.browser.element(self, parent=self.parent).value_of_css_property('color'),
+            'unknown'
+        )
+
+    def read(self):
+        """Returns current icon color"""
+        return self.color
+
+
 class VirtwhoConfiguresView(BaseLoggedInView, SearchableViewMixin):
     title = Text("//h1[text()='Virt-who Configurations']")
     new = Text("//a[contains(@href, '/foreman_virt_who_configure/configs/new')]")
@@ -26,6 +67,7 @@ class VirtwhoConfiguresView(BaseLoggedInView, SearchableViewMixin):
         './/table',
         column_widgets={
             'Name': Text('./a'),
+            'Status': StatusIcon(),
             'Actions': ActionsDropdown("./div[contains(@class, 'btn-group')]"),
         }
     )
@@ -134,7 +176,7 @@ class VirtwhoConfigureDetailsView(BaseLoggedInView):
 
     @View.nested
     class overview(SatTab):
-        status = Text('.//span[contains(@class,"virt-who-config-report-status")]')
+        status = StatusIcon()
         hypervisor_type = Text('.//span[contains(@class,"config-hypervisor_type")]')
         hypervisor_server = Text('.//span[contains(@class,"config-hypervisor_server")]')
         hypervisor_username = Text('.//span[contains(@class,"config-hypervisor_username")]')
