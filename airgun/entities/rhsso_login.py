90f2543b70eecb4b03b1d5e093395bfd8c54c5c1
omkarkhatavkar <okhatavkar007@gmail.com>
2020-03-17 19:58:52 +0530

    precommit fix and added login again function

--- airgun/entities/rhsso_login.py
+++ airgun/entities/rhsso_login.py
@@ -1,9 +1,9 @@
 from airgun.entities.base import BaseEntity
 from airgun.navigation import NavigateStep
 from airgun.navigation import navigator
-from airgun.views.rhsso_login import RhssoLoginView
-from airgun.views.rhsso_login import RhssoExtLogoutView
 from airgun.views.common import BaseLoggedInView
+from airgun.views.rhsso_login import RhssoExtLogoutView
+from airgun.views.rhsso_login import RhssoLoginView
 
 
 class RHSSOLoginEntity(BaseEntity):
@@ -13,13 +13,17 @@ class RHSSOLoginEntity(BaseEntity):
         view.fill(values)
         view.submit.click()
 
+    def login_again(self):
+        view = RhssoExtLogoutView(self.browser)
+        view.login_again.click()
+        return view.read()
+
     def logout(self):
         view = BaseLoggedInView(self.browser)
         view.taxonomies.select_logout()
         view.flash.assert_no_error()
         view.flash.dismiss()
         view = RhssoExtLogoutView(self.browser)
-        view.login_again.click()
         return view.read()
 
 
