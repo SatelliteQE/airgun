6fedb22cc6b5687b918795bc8f6f699e65cbf748
Lukáš Hellebrandt <lhellebr@redhat.com>
2020-05-18 20:11:11 +0200

    flake8 complained about f-string without placeholders (#486)

--- airgun/widgets.py
+++ airgun/widgets.py
@@ -2182,7 +2182,7 @@ class PopOverWidget(Widget):
                     widget.fill(item)
                     break
         else:
-            raise ReadOnlyWidgetError(f'This field setting is read-only')
+            raise ReadOnlyWidgetError('This field setting is read-only')
         self.submit.click()
 
     def read(self):
