7d236f27a7a047b5af44baf90dd7fd2fab554162
Mirek Długosz (Mirosław Długosz-Zalewski) <mzalewsk@redhat.com>
2020-06-09 07:43:34 +0200

    Convert <a> to <input> on Job page (#493) (#497)
    (cherry picked from commit 38ffc7965955691a9f05c99b297e23a430cd6dbb)

--- airgun/views/job_invocation.py
+++ airgun/views/job_invocation.py
@@ -8,6 +8,7 @@ from widgetastic.widget import (
     View,
 )
 from widgetastic_patternfly import BreadCrumb
+from widgetastic_patternfly import Button
 
 from airgun.views.common import (
     BaseLoggedInView,
@@ -190,8 +191,8 @@ class JobInvocationStatusView(BaseLoggedInView):
     rerun = Text("//a[text()='Rerun']")
     rerun_failed = Text("//a[text()='Rerun failed']")
     job_task = Text("//a[text()='Job Task']")
-    cancel_job = Text("//a[text()='Cancel Job']")
-    abort_job = Text("//a[text()='Abort Job']")
+    cancel_job = Button(value='Cancel Job')
+    abort_job = Button(value='Abort Job')
 
     @View.nested
     class overview(SatTab):
