44056d127694083b618f5082b0c50d6103b92b5a
Shubham Ganar <shubhamsg123m@gmail.com>
2022-06-28 17:15:22 +0530

    Remove unnecessary handle_alert

--- airgun/entities/user.py
+++ airgun/entities/user.py
@@ -49,7 +49,6 @@ class UserEntity(BaseEntity):
         view = self.navigate_to(self, 'All')
         view.search(entity_name)
         view.table.row(username=entity_name)['Actions'].widget.click(handle_alert=True)
-        self.browser.handle_alert()
         view.flash.assert_no_error()
         view.flash.dismiss()
 
