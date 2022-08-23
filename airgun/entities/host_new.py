704cfbc635dd54801a8bcb86f7ebf73a60172a1f
Peter Dragun <43444182+peterdragun@users.noreply.github.com>
2022-08-23 16:29:41 +0200

    Add content tab to new host UI (#720)
    * add content view
    
    * add all filers and locators from 6.12
    
    * add repo sets tab
    
    * add install packages view
    
    * add repository sets view
    
    * add entities to new host UI content tab
    
    * ensure details page displayed
    
    * fix module stream dropdown
    
    * fix errata apply dropdown
    
    * use lowercase card names
    
    * update status locators
    
    * add parent context to host details card
    
    * add BZ link

--- airgun/entities/host_new.py
+++ airgun/entities/host_new.py
@@ -1,6 +1,8 @@
 from airgun.entities.host import HostEntity
 from airgun.navigation import NavigateStep
 from airgun.navigation import navigator
+from airgun.views.host_new import InstallPackagesView
+from airgun.views.host_new import ModuleStreamDialog
 from airgun.views.host_new import NewHostDetailsView
 
 
@@ -22,6 +24,87 @@ class NewHostEntity(HostEntity):
         """
         view = self.navigate_to(self, 'NewDetails', entity_name=entity_name)
         return view.read(widget_names=widget_names)
+
+    def get_packages(self, entity_name, search=""):
+        """Filter installed packages on host"""
+        view = self.navigate_to(self, 'NewDetails', entity_name=entity_name)
+        view.content.packages.select()
+        view.content.packages.searchbar.fill(search)
+        # wait for filter to apply
+        self.browser.plugin.ensure_page_safe()
+        return view.content.packages.read()
+
+    def install_package(self, entity_name, package):
+        """Installs package on host using the installation modal"""
+        view = self.navigate_to(self, 'NewDetails', entity_name=entity_name)
+        view.wait_displayed()
+        view.content.packages.select()
+        view.content.packages.dropdown.wait_displayed()
+        view.content.packages.dropdown.item_select('Install packages')
+        view = InstallPackagesView(self.browser)
+        view.wait_displayed()
+        view.searchbar.fill(package)
+        # wait for filter to apply
+        self.browser.plugin.ensure_page_safe()
+        view.select_all.click()
+        view.install.click()
+
+    def apply_package_action(self, entity_name, package_name, action):
+        """Apply `action` to selected package based on the `package_name`"""
+        view = self.navigate_to(self, 'NewDetails', entity_name=entity_name)
+        view.wait_displayed()
+        view.content.packages.searchbar.fill(package_name)
+        # wait for filter to apply
+        self.browser.plugin.ensure_page_safe()
+        view.content.packages.table.wait_displayed()
+        view.content.packages.table[0][5].widget.item_select(action)
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
+    def get_errata_by_type(self, entity_name, type):
+        """List errata based on type and return table"""
+        view = self.navigate_to(self, 'NewDetails', entity_name=entity_name)
+        view.wait_displayed()
+        view.content.errata.select()
+        view.content.errata.type_filter.fill(type)
+        return view.read(widget_names="content.errata.table")
+
+    def apply_erratas(self, entity_name, search):
+        """Apply errata on selected host based on errata_id"""
+        view = self.navigate_to(self, 'NewDetails', entity_name=entity_name)
+        view.wait_displayed()
+        view.content.errata.searchbar.fill(search)
+        # wait for filter to apply
+        self.browser.plugin.ensure_page_safe()
+        view.content.errata.select_all.click()
+        view.content.errata.apply.click()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
+    def get_module_streams(self, entity_name, search):
+        """Filter module streams"""
+        view = self.navigate_to(self, 'NewDetails', entity_name=entity_name)
+        view.wait_displayed()
+        view.content.module_streams.select()
+        view.content.module_streams.searchbar.fill(search)
+        # wait for filter to apply
+        self.browser.plugin.ensure_page_safe()
+        view.content.module_streams.table.wait_displayed()
+        return view.content.module_streams.table.read()
+
+    def apply_module_streams_action(self, entity_name, module_stream, action):
+        """Apply `action` to selected Module stream based on the `module_stream`"""
+        view = self.navigate_to(self, 'NewDetails', entity_name=entity_name)
+        view.wait_displayed()
+        view.content.module_streams.searchbar.fill(module_stream)
+        # wait for filter to apply
+        self.browser.plugin.ensure_page_safe()
+        view.content.module_streams.table[0][5].widget.item_select(action)
+        modal = ModuleStreamDialog(self.browser)
+        if modal.is_displayed:
+            modal.confirm()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
 
 
 @navigator.register(NewHostEntity, 'NewDetails')
--- airgun/views/host_new.py
+++ airgun/views/host_new.py
@@ -1,12 +1,21 @@
+from widgetastic.widget import Checkbox
 from widgetastic.widget import Text
+from widgetastic.widget import TextInput
 from widgetastic.widget import View
 from widgetastic.widget import Widget
+from widgetastic.widget.table import Table
+from widgetastic_patternfly4 import Button
 from widgetastic_patternfly4 import Dropdown
+from widgetastic_patternfly4 import Pagination
+from widgetastic_patternfly4 import Select
 from widgetastic_patternfly4 import Tab
 from widgetastic_patternfly4.ouia import BreadCrumb
-from widgetastic_patternfly4.ouia import Button
+from widgetastic_patternfly4.ouia import Button as OUIAButton
+from widgetastic_patternfly4.ouia import ExpandableTable
+from widgetastic_patternfly4.ouia import PatternflyTable
 
 from airgun.views.common import BaseLoggedInView
+from airgun.widgets import Pf4ConfirmationDialog
 
 
 class Card(View):
@@ -15,21 +24,25 @@ class Card(View):
     title = Text('.//div[@class="pf-c-card__title"]')
 
 
+class DropdownWithDescripton(Dropdown):
+    """Dropdown with description below items"""
+
+    ITEM_LOCATOR = ".//*[contains(@class, 'pf-c-dropdown__menu-item') and contains(text(), {})]"
+
+
 class HostDetailsCard(Widget):
     """Details card body contains multiple host detail information"""
 
-    LABELS = './/div[@class="pf-c-description-list__group"]//dt//span'
-    VALUES = (
-        './/div[@class="pf-c-description-list__group"]//dd//descendant::*/normalize-space(.)/..'
-    )
+    LABELS = '//div[@class="pf-c-description-list__group"]//dt//span'
+    VALUES = '//div[@class="pf-c-description-list__group"]//dd//descendant::*/text()/..'
 
     def read(self):
         """Return a dictionary where keys are property names and values are property values.
         Values are either in span elements or in div elements
         """
         items = {}
-        labels = self.browser.elements(self.LABELS)
-        values = self.browser.elements(self.VALUES)
+        labels = self.browser.elements(f'{self.parent.ROOT}{self.LABELS}')
+        values = self.browser.elements(f'{self.parent.ROOT}{self.VALUES}')
         # the length of elements should be always same
         if len(values) != len(labels):
             raise AttributeError(
@@ -49,54 +62,196 @@ class NewHostDetailsView(BaseLoggedInView):
 
     @property
     def is_displayed(self):
-        breadcrumb_loaded = self.browser.wait_for_element(
-            self.Overview.RecentJobsCard.is_table_loaded, exception=False
-        )
+        breadcrumb_loaded = self.browser.wait_for_element(self.breadcrumb, exception=False)
         return breadcrumb_loaded and self.breadcrumb.locations[0] == 'Hosts'
 
-    edit = Button('OUIA-Generated-Button-secondary-1')
+    edit = OUIAButton('OUIA-Generated-Button-secondary-1')
     dropdown = Dropdown(locator='//button[@id="hostdetails-kebab"]/..')
 
     @View.nested
-    class Overview(Tab):
+    class overview(Tab):
         ROOT = './/div[contains(@class, "host-details-tab-item")]'
 
         @View.nested
-        class DetailsCard(Card):
-            ROOT = '(.//article[contains(@class, "pf-c-card")])[1]'
+        class details(Card):
+            ROOT = './/article[.//div[text()="Details"]]'
             details = HostDetailsCard()
 
         @View.nested
-        class HostStatusCard(Card):
-            ROOT = '(//article[contains(@class, "pf-c-card")])[2]'
+        class host_status(Card):
+            ROOT = './/article[.//span[text()="Host status"]]'
             status = Text('.//h4[contains(@data-ouia-component-id, "OUIA-Generated-Title")]')
 
-            status_success = Text('.//span[@class="status-success"]')
-            status_warning = Text('.//span[@class="status-warning"]')
-            status_error = Text('.//span[@class="status-error"]')
-            status_disabled = Text('.//span[@class="disabled"]')
+            status_success = Text('.//a[span[@class="status-success"]]')
+            status_warning = Text('.//a[span[@class="status-warning"]]')
+            status_error = Text('.//a[span[@class="status-error"]]')
+            status_disabled = Text('.//a[span[@class="disabled"]]')
 
         @View.nested
-        class RecentJobsCard(Card):
-            ROOT = '(//article[contains(@class, "pf-c-card")])[5]'
+        class installable_errata(Card):
+            ROOT = './/article[.//div[text()="Installable errata"]]'
+
+            security_advisory = Text('.//a[contains(@href, "type=security")]')
+            bug_fixes = Text('.//a[contains(@href, "type=bugfix")]')
+            enhancements = Text('.//a[contains(@href, "type=enhancement")]')
+
+        @View.nested
+        class total_risks(Card):
+            ROOT = './/article[.//div[text()="Total risks"]]'
+
+            low = Text('.//*[@id="legend-labels-0"]/*')
+            moderate = Text('.//*[@id="legend-labels-1"]/*')
+            important = Text('.//*[@id="legend-labels-2"]/*')
+            critical = Text('.//*[@id="legend-labels-3"]/*')
+
+        @View.nested
+        class recent_jobs(Card):
+            ROOT = './/article[.//div[text()="Recent jobs"]]'
             is_table_loaded = './/ul[@aria-label="recent-jobs-table"]'
 
     @View.nested
-    class Content(Tab):
+    class content(Tab):
+        # TODO Setting ROOT is just a workaround because of BZ 2119076,
+        # once this gets fixed we should use the parametrized locator from Tab class
+        ROOT = './/div'
+
+        @View.nested
+        class packages(Tab):
+            # workaround for BZ 2119076
+            ROOT = './/div[@id="packages-tab"]'
+
+            select_all = Checkbox(locator='.//div[@id="selection-checkbox"]/div/label')
+            searchbar = TextInput(locator='.//input[contains(@class, "pf-m-search")]')
+            status_filter = Dropdown(locator='.//div[@aria-label="select Status container"]/div')
+            upgrade = Button(locator='.//button[normalize-space(.)="Upgrade"]')
+            dropdown = Dropdown(locator='.//div[button[@aria-label="bulk_actions"]]')
+
+            table = PatternflyTable(
+                component_id="host-packages-table",
+                column_widgets={
+                    0: Checkbox(locator='.//input[@type="checkbox"]'),
+                    'Package': Text('./parent::td'),
+                    'Status': Text('./span'),
+                    'Installed version': Text('./parent::td'),
+                    'Upgradable to': Text('./span'),
+                    5: Dropdown(locator='.//div[contains(@class, "pf-c-dropdown")]'),
+                },
+            )
+            pagination = Pagination()
+
+        @View.nested
+        class errata(Tab):
+            # workaround for BZ 2119076
+            ROOT = './/div[@id="errata-tab"]'
+
+            select_all = Checkbox(locator='.//div[@id="selection-checkbox"]/div/label')
+            searchbar = TextInput(locator='.//input[contains(@class, "pf-m-search")]')
+            type_filter = Select(locator='.//div[@aria-label="select Type container"]/div')
+            severity_filter = Select(locator='.//div[@aria-label="select Severity container"]/div')
+            apply = Button(locator='.//button[normalize-space(.)="Apply"]')
+            dropdown = Dropdown(locator='.//div[button[@aria-label="bulk_actions"]]')
+
+            table = ExpandableTable(
+                component_id="host-errata-table",
+                column_widgets={
+                    1: Checkbox(locator='.//input[@type="checkbox"]'),
+                    'Errata': Text('./a'),
+                    'Type': Text('./span'),
+                    'Severity': Text('./span'),
+                    'Installable': Text('./span'),
+                    'Synopsis': Text('./span'),
+                    'Published date': Text('./span/span'),
+                    8: Dropdown(locator='./div'),
+                },
+            )
+            pagination = Pagination()
+
+        @View.nested
+        class module_streams(Tab):
+            TAB_NAME = 'Module streams'
+            # workaround for BZ 2119076
+            ROOT = './/div[@id="modulestreams-tab"]'
+
+            searchbar = TextInput(locator='.//input[contains(@class, "pf-m-search")]')
+            status_filter = Select(locator='.//div[@aria-label="select Status container"]/div')
+            installation_status_filter = Select(
+                locator='.//div[@aria-label="select Installation status container"]/div'
+            )
+            dropdown = Dropdown(locator='.//div[button[@aria-label="bulk_actions"]]')
+
+            table = Table(
+                locator='.//table[@aria-label="Content View Table"]',
+                column_widgets={
+                    'Name': Text('./a'),
+                    'State': Text('.//span'),
+                    'Stream': Text('./parent::td'),
+                    'Installation status': Text('.//small'),
+                    'Installed profile': Text('./parent::td'),
+                    5: DropdownWithDescripton(locator='.//div[contains(@class, "pf-c-dropdown")]'),
+                },
+            )
+            pagination = Pagination()
+
+        @View.nested
+        class repository_sets(Tab):
+            TAB_NAME = 'Repository sets'
+            # workaround for BZ 2119076
+            ROOT = './/div[@id="repo-sets-tab"]'
+
+            select_all = Checkbox(locator='.//div[@id="selection-checkbox"]/div/label')
+            searchbar = TextInput(locator='.//input[contains(@class, "pf-m-search")]')
+            status_filter = Select(locator='.//div[@aria-label="select Status container"]/div')
+            dropdown = Dropdown(locator='.//div[button[@aria-label="bulk_actions"]]')
+
+            table = Table(
+                locator='.//table[@aria-label="Content View Table"]',
+                column_widgets={
+                    0: Checkbox(locator='.//input[@type="checkbox"]'),
+                    'Repository': Text('./span'),
+                    'Product': Text('./a'),
+                    'Repository path': Text('./span'),
+                    'Status': Text('.//span[contains(@class, "pf-c-label__content")]'),
+                    5: Dropdown(locator='.//div[contains(@class, "pf-c-dropdown")]'),
+                },
+            )
+            pagination = Pagination()
+
+    @View.nested
+    class traces(Tab):
+        enable_traces = OUIAButton('OUIA-Generated-Button-primary-1')
+
+    @View.nested
+    class ansible(Tab):
         pass
 
     @View.nested
-    class Traces(Tab):
-        enable_traces = Button('OUIA-Generated-Button-primary-1')
-
-    @View.nested
-    class RepositorySets(Tab):
+    class insights(Tab):
         pass
 
-    @View.nested
-    class Ansible(Tab):
-        pass
 
-    @View.nested
-    class Insights(Tab):
-        pass
+class InstallPackagesView(View):
+    """Install packages modal"""
+
+    ROOT = './/div[@id="package-install-modal"]'
+
+    select_all = Checkbox(locator='.//div[@id="selection-checkbox"]/div/label')
+    searchbar = TextInput(locator='.//input[contains(@class, "pf-m-search")]')
+
+    table = Table(
+        locator='.//table[@aria-label="Content View Table"]',
+        column_widgets={
+            0: Checkbox(locator='.//input[@type="checkbox"]'),
+            'Package': Text('./parent::td'),
+            'Version': Text('./parent::td'),
+        },
+    )
+    pagination = Pagination()
+
+    install = Button(locator='.//button[(normalize-space(.)="Install")]')
+    cancel = Button('Cancel')
+
+
+class ModuleStreamDialog(Pf4ConfirmationDialog):
+
+    confirm_dialog = Button(locator='.//button[@aria-label="confirm-module-action"]')
+    cancel_dialog = Button(locator='.//button[@aria-label="cancel-module-action"]')
