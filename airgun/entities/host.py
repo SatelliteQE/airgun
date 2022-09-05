06b1dd8b9ce2adcbecb4f55ccbd18564465ba6f5
Peter Dragun <pdragun@redhat.com>
2022-09-05 12:20:40 +0200

    ensure cockpit page is safe before locating elements

--- airgun/entities/host.py
+++ airgun/entities/host.py
@@ -259,6 +259,7 @@ class HostEntity(BaseEntity):
 
         # switch to the last opened tab,
         self.browser.switch_to_window(self.browser.window_handles[-1])
+        self.browser.plugin.ensure_page_safe()
         self.browser.wait_for_element(locator='//div[@id="content"]/iframe', exception=True)
         # the remote host content is loaded in an iframe, let's switch to it
         self.browser.switch_to_frame(locator='//div[@id="content"]/iframe')
