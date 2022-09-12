c5c86b95c63e8a73ab7e14506f5a37e76eecbcc7
Peter Dragun <43444182+peterdragun@users.noreply.github.com>
2022-09-12 20:02:59 +0200

    Ensure cockpit page is safe before locating elements (#743)
    * ensure cockpit page is safe before locating elements
    
    * find_elements fix

--- airgun/entities/host.py
+++ airgun/entities/host.py
@@ -247,9 +247,8 @@ class HostEntity(BaseEntity):
         view.validations.assert_no_errors()
         # switch to the last opened tab,
         self.browser.switch_to_window(self.browser.window_handles[-1])
-        self.browser.wait_for_element(
-            locator='//div[@id="index-brand"]', exception=True, timeout=10
-        )
+        self.browser.plugin.ensure_page_safe()
+        self.browser.wait_for_element(locator='//div[@id="content"]/iframe', exception=True)
         # the remote host content is loaded in an iframe, let's switch to it
         self.browser.switch_to_frame(locator='//div[@id="content"]/iframe')
 
@@ -259,7 +258,8 @@ class HostEntity(BaseEntity):
         hostname_button_view = self.browser.selenium.find_elements_by_id(
             'system_information_hostname_button'
         )
-        hostname = hostname_button_view[0].text
+        hostname_button = self.browser.selenium.find_elements("id", hostname_id)
+        hostname = hostname_button[0].text
         self.browser.switch_to_main_frame()
         self.browser.switch_to_window(self.browser.window_handles[0])
         self.browser.close_window(self.browser.window_handles[-1])
