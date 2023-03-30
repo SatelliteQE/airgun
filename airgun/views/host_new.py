100c207a6340e19952543bffe568cdc4b9273d22
Ladislav Vasina <lvasina@redhat.com>
2023-03-30 07:32:56 +0200

    View for multiple host tabs added

--- airgun/views/host_new.py
+++ airgun/views/host_new.py
@@ -15,9 +15,12 @@ from widgetastic_patternfly4 import Tab
 from widgetastic_patternfly4.ouia import BreadCrumb
 from widgetastic_patternfly4.ouia import Button as OUIAButton
 from widgetastic_patternfly4.ouia import ExpandableTable
+from widgetastic_patternfly4.ouia import Modal
 from widgetastic_patternfly4.ouia import PatternflyTable
 
 from airgun.views.common import BaseLoggedInView
+from airgun.widgets import Accordion
+from airgun.widgets import ItemsList
 from airgun.widgets import CheckboxGroup
 from airgun.widgets import Pf4ActionsDropdown
 from airgun.widgets import Pf4ConfirmationDialog
@@ -34,6 +37,27 @@ class SearchInput(TextInput):
         return changed
 
 
+class RemediationView(Modal):
+    """Remediation window view"""
+
+    OUIA_ID = 'OUIA-Generated-Modal-large-1'
+    remediate = Button('Remediate')
+    cancel = Button('Cancel')
+    table = PatternflyTable(
+        component_id='OUIA-Generated-Table-4',
+        column_widgets={
+            'Hostname': Text('.//td[1]'),
+            'Recommendation': Text('.//td[2]'),
+            'Resolution': Text('.//td[3]'),
+            'Reboot Required': Text('.//td[4]'),
+        },
+    )
+
+    @property
+    def is_displayed(self):
+        return self.title.wait_displayed()
+
+
 class Card(View):
     """Each card in host view has it's own title with same locator"""
 
@@ -47,7 +71,7 @@ class DropdownWithDescripton(Dropdown):
 
 
 class HostDetailsCard(Widget):
-    """Details card body contains multiple host detail information"""
+    """Overview/Details and Details/SystemProperties card body contains multiple host detail information"""
 
     LABELS = '//div[@class="pf-c-description-list__group"]//dt//span'
     VALUES = '//div[@class="pf-c-description-list__group"]//dd//descendant::*/text()/..'
@@ -92,12 +116,17 @@ class NewHostDetailsView(BaseLoggedInView):
         @View.nested
         class details(Card):
             ROOT = './/article[.//div[text()="Details"]]'
+
             details = HostDetailsCard()
+
+            #TODO  power_operations = 
 
         @View.nested
         class host_status(Card):
             ROOT = './/article[.//span[text()="Host status"]]'
+
             status = Text('.//h4[contains(@data-ouia-component-id, "global-state-title")]')
+            manage_all_statuses = Text('.//a[normalize-space(.)="Manage all statuses"]')
 
             status_success = Text('.//a[span[@class="status-success"]]')
             status_warning = Text('.//a[span[@class="status-warning"]]')
@@ -109,6 +138,26 @@ class NewHostDetailsView(BaseLoggedInView):
 
             all_audits = Text('.//a[normalize-space(.)="All audits"]')
             table = SatTableWithoutHeaders(locator='.//table[@aria-label="audits table"]')
+        
+
+        @View.nested
+        class recent_communication(Card):
+            ROOT = './/article[.//div[text()="Recent communication"]]'
+
+            last_checkin_value = Text('.//div[@class="pf-c-description-list__text"]')
+
+        @View.nested
+        class errata(Card):
+            ROOT = './/article[.//div[text()="Errata"]]'
+
+            enable_repository_sets = Text('.//a[normalize-space(.)="Enable repository sets"]')
+
+        @View.nested
+        class content_view_details(Card):
+            ROOT = './/article[.//div[text()="Content view details"]]'
+            actions = Dropdown(locator='.//div[contains(@class, "pf-c-dropdown")]')
+
+            org_view = Text('.//a[contains(@href, "content_views")]')
 
         @View.nested
         class installable_errata(Card):
@@ -121,6 +170,7 @@ class NewHostDetailsView(BaseLoggedInView):
         @View.nested
         class total_risks(Card):
             ROOT = './/article[.//div[text()="Total risks"]]'
+            actions = Dropdown(locator='.//div[contains(@class, "pf-c-dropdown")]')
 
             low = Text('.//*[@id="legend-labels-0"]/*')
             moderate = Text('.//*[@id="legend-labels-1"]/*')
@@ -140,6 +190,107 @@ class NewHostDetailsView(BaseLoggedInView):
 
             class scheduled(Tab):
                 table = SatTableWithoutHeaders(locator='.//table[@aria-label="recent-jobs-table"]')
+
+        @View.nested
+        class system_purpose(Card):
+            ROOT = './/article[.//div[text()="System purpose"]]'
+            edit_system_purpose = Text('.//button[@data-ouia-component-id="syspurpose-edit-button"]')
+
+            role = Text('.//dd[contains(@class, "pf-c-description-list__description")][1]')
+            sla = Text('.//dd[contains(@class, "pf-c-description-list__description")][2]')
+            usage_type = Text('.//dd[contains(@class, "pf-c-description-list__description")][3]')
+            release_version = Text('.//dd[contains(@class, "pf-c-description-list__description")][4]')
+            addons = Text('.//dd[contains(@class, "pf-c-description-list__description")][5]')
+
+
+    @View.nested
+    class details(Tab):
+        ROOT = './/div[contains(@class, "host-details-tab-item")]'
+
+        card_collapse_switch = Text('.//button[contains(@data-ouia-component-id, "expand-button")]')
+
+        @View.nested
+        class system_properties(Card):
+            ROOT = './/article[.//div[text()="System properties"]]'
+
+            sys_properties = HostDetailsCard()
+
+        @View.nested
+        class operating_system(Card):
+            ROOT = './/article[.//div[text()="Operating system"]]'
+
+            architecture = Text('.//a[contains(@data-ouia-component-id, "OUIA-Generated-Button-link-1")]')
+            os = Text('.//a[contains(@data-ouia-component-id, "OUIA-Generated-Button-link-2")]')
+            boot_time = Text('.//div[contains(@class, "pf-c-description-list__group")][3]/dd/div')
+            kernel_release = Text('.//div[contains(@class, "pf-c-description-list__group")][4]/dd/div')
+
+        @View.nested
+        class provisioning(Card):
+            ROOT = './/article[.//div[text()="Provisioning"]]'
+
+            build_duration = Text('.//div[contains(@class, "pf-c-description-list__group")][1]/dd/div')
+            token = Text('.//div[contains(@class, "pf-c-description-list__group")][2]/dd/div')
+            pxe_loader = Text('.//div[contains(@class, "pf-c-description-list__group")][3]/dd/div')
+
+        @View.nested
+        class bios(Card):
+            ROOT = './/article[.//div[text()="BIOS"]]'
+
+            vendor = Text('.//div[contains(@class, "pf-c-description-list__group")][1]/dd/div')
+            version = Text('.//div[contains(@class, "pf-c-description-list__group")][2]/dd/div')
+            release_date = Text('.//div[contains(@class, "pf-c-description-list__group")][3]/dd/div')
+
+        @View.nested
+        class registration_details(Card):
+            ROOT = './/article[.//div[text()="Registration details"]]'
+
+            registered_on = Text('.//div[contains(@class, "pf-c-description-list__group")][1]/dd/div')
+            registered_by = ItemsList('.//div[contains(@class, "pf-c-description-list__group")][2]/ul')
+            activation_key_link = Text('.//div[contains(@class, "pf-c-description-list__group")][2]//a')
+            registered_through = Text('.//div[contains(@class, "pf-c-description-list__group")][3]/dd/div')
+
+        @View.nested
+        class hw_properties(Card):
+            ROOT = './/article[.//div[text()="HW properties"]]'
+
+            model = Text('.//div[contains(@class, "pf-c-description-list__group")][1]//dd')
+            number_of_cpus = Text('.//div[contains(@class, "pf-c-description-list__group")][2]//dd')
+            sockets = Text('.//div[contains(@class, "pf-c-description-list__group")][3]//dd')
+            cores_per_socket = Text('.//div[contains(@class, "pf-c-description-list__group")][4]//dd')
+            ram = Text('.//div[contains(@class, "pf-c-description-list__group")][5]//dd')
+
+        @View.nested
+        class provisioning_templates(Card):
+            ROOT = './/article[.//div[text()="Provisioning templates"]]'
+
+            templates_table = SatTableWithoutHeaders(locator='.//table[@aria-label="templates table"]')
+
+        @View.nested
+        class installed_products(Card):
+            ROOT = './/article[.//div[text()="Installed products"]]'
+
+            installed_products_list = ItemsList(locator='.//ul[contains(@class, "pf-c-list")]')
+
+        @View.nested
+        class networking_interfaces(Card):
+            ROOT = './/article[.//div[text()="Networking interfaces"]]'
+
+            # TODO Finish Accordion correctly
+            networking_interfaces_accordion = Accordion(locator='.//div[contains(@class, "pf-c-card__expandable-content")]')
+            #networking_interfaces_accordion = Accordion(locator='.//dl[contains(@class, "pf-c-accordion interface-accordion")]')
+            networking_interfaces_dict = {
+                'fqdn': Text('.//div[contains(@class, "pf-c-accordion__expanded-content-body")]//div[.//dt[normalize-space(.)="FQDN"]]//div'),
+                'ipv4': Text('.//div[contains(@class, "pf-c-accordion__expanded-content-body")]//div[.//dt[normalize-space(.)="IPv4"]]//div'),
+                'ipv6': Text('.//div[contains(@class, "pf-c-accordion__expanded-content-body")]//div[.//dt[normalize-space(.)="IPv6"]]//div'),
+                'mac': Text('.//div[contains(@class, "pf-c-accordion__expanded-content-body")]//div[.//dt[normalize-space(.)="MAC"]]//div'),
+                'subnet': Text('.//div[contains(@class, "pf-c-accordion__expanded-content-body")]//div[.//dt[normalize-space(.)="Subnet"]]//div'),
+                'mtu': Text('.//div[contains(@class, "pf-c-accordion__expanded-content-body")]//div[.//dt[normalize-space(.)="MTU"]]//div')
+            }
+            edit_interfaces = Text('.//a[contains(@href, "/hosts/")]')
+
+        @View.nested
+        class networking_interface(Card):
+            pass    
 
     @View.nested
     class content(Tab):
@@ -230,7 +381,7 @@ class NewHostDetailsView(BaseLoggedInView):
         class repository_sets(Tab):
             TAB_NAME = 'Repository sets'
             # workaround for BZ 2119076
-            ROOT = './/div[@id="repo-sets-tab"]'
+            ROOT = './/div[@id="repo-sets-tab"]'    
 
             select_all = Checkbox(locator='.//div[@id="selection-checkbox"]/div/label')
             searchbar = SearchInput(locator='.//input[contains(@class, "pf-m-search")]')
@@ -251,15 +402,47 @@ class NewHostDetailsView(BaseLoggedInView):
             pagination = Pagination()
 
     @View.nested
+    class parameters(Tab):
+        ROOT = './/div'
+
+        add_parameter = OUIAButton('OUIA-Generated-Button-primary-4')
+        searchbar = SearchInput(locator='//input[contains(@class, "pf-c-search-input__text-input")]')
+        # TODO solve case while adding new parameter. I need to somehow specify if that cell in that column should be Text() or TextInput() etc.
+        parameters_table = Table(
+            locator='.//table[@aria-label="Parameters table"]',
+            column_widgets={
+                'Name': Text('.//td[contains(@data-label, "Name")]'),
+                'Type': Text('.//td[contains(@data-label, "Type")]'),
+                'Value': Text('.//td[contains(@data-label, "Value")]'),
+                'Source': Text('.//td[contains(@data-label, "Source")]'),
+                4: Button(locator='.//button[contains(@data-ouia-component-id, "OUIA-Generated-Button-plain-")]'),
+                5: Button(locator='.//td[contains(@class, "parameters-actions")]//button'),
+            }
+        )
+        pagination = Pagination()
+
+    @View.nested
     class traces(Tab):
         ROOT = './/div'
 
         title = Text('//h2')
         enable_traces = OUIAButton('enable-traces-button')
-
-    @View.nested
-    class insights(Tab):
-        pass
+        select_all = Checkbox(locator='.//input[contains(@aria-label, "Select all")]')
+        searchbar = SearchInput(locator='.//input[contains(@aria-label, "Select all")]')
+        Pf4ActionsDropdown = Button(locator='.//div[contains(@aria-label, "bulk_actions_dropdown")]')
+        traces_table = PatternflyTable(
+            #locator='.//table[contains(@class, "pf-c-table")]',
+            component_id='host-traces-table',
+            column_widgets={
+            0: Checkbox(locator='.//input[contains(@aria-label, "Select row")]'),
+            'Application': Text('.//td[2]'),
+            'Type': Text('.//td[3]'),
+            'Helper': Text('.//td[4]'),
+            4: Button(locator='.//button[contains(@aria-label, "Actions")]')
+            }
+        )
+        pagination = Pagination()
+        
 
     @View.nested
     class ansible(Tab):
@@ -369,6 +552,50 @@ class NewHostDetailsView(BaseLoggedInView):
                 @property
                 def is_displayed(self):
                     return self.previousText.is_displayed
+
+    @View.nested
+    class reports(Tab):
+        ROOT = './/div'
+
+        search_bar = SearchInput(locator='.//input[contains(@class, "search-input")]')
+        reports_table = PatternflyTable(
+            component_id='reports-table',
+            column_widgets={
+                'reported_at': Text('.//a'),
+                'failed': Text('.//td[2]'),
+                'failed_restarts': Text('.//td[3]'),
+                'restarted': Text('.//td[4]'),
+                'applied': Text('.//td[5]'),
+                'skipped': Text('.//td[6]'),
+                'origin': Text('.//td[7]'),
+                'pending': Text('.//td[8]'),
+                8: Button(locator='.//button[contains(@aria-label, "Actions")]'),
+            }
+        )
+
+        pagination = Pagination()
+
+    @View.nested
+    class insights(Tab):
+        ROOT = './/div'
+
+        search_bar = SearchInput(locator='.//input[contains(@class, "search-input")]')
+        remediate = OUIAButton('OUIA-Generated-Button-primary-5')
+        insights_dropdown = Dropdown(locator='.//div[contains(@class, "insights-dropdown")]')
+
+        select_all = Checkbox(locator='.//input[@name="check-all"]')
+        recommendations_table = PatternflyTable(
+            component_id='OUIA-Generated-Table-2',
+            column_widgets={
+                0: Checkbox(locator='.//input[@type="checkbox"]'),
+                'Recommendation': Text('.//td[2]'),
+                'Total Risk': Text('.//td[3]'),
+                'Remediate': Text('.//td[4]'),
+                4: Button(locator='.//button[contains(@aria-label, "Actions")]')
+            }
+        )
+        pagination = Pagination()
+        remediation_window = View.nested(RemediationView)
 
 
 class InstallPackagesView(View):
