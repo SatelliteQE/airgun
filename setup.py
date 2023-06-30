b0d227456ae748658843443dd3a50cdec7a9f305
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2023-06-30 09:09:31 -0400

    Bump GHA python version check to 3.10 and 3.11 (#877) (#878)
    * Bump GHA python version check to 3.10 and 3.11
    
    * Make list items string
    
    * Update setup.py to reflect supported version
    
    (cherry picked from commit 88a9f225088ca84ec865e10926522eb2fbf58ea8)
    
    Co-authored-by: Ladislav Vašina <lvasina@redhat.com>

--- setup.py
+++ setup.py
@@ -39,8 +39,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: POSIX :: LinuxProgramming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
