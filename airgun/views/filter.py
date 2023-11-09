287a933619fb09cd5e2f77491d07d3e6a61281ca
Matyáš Strelec <mstrelec@redhat.com>
2023-11-09 19:01:47 +0100

    Pagination refactor (#947)
    * update views to use Widgetastic's PF4 Pagination
    
    * rename to PF4Pagination
    
    * fix SatTable locator
    
    * add Pagination to location for testing
    
    * pre-commit
    
    (cherry picked from commit cd24e60147bca6183c561e513bdebfc1509d4494)

--- airgun/views/filter.py
+++ airgun/views/filter.py
@@ -7,12 +7,12 @@ from widgetastic.widget import (
     View,
 )
 from widgetastic_patternfly import BreadCrumb
+from widgetastic_patternfly4 import Pagination as PF4Pagination
 
 from airgun.views.common import BaseLoggedInView, SatTab
 from airgun.widgets import (
     ActionsDropdown,
     MultiSelect,
-    Pagination,
     PF4FilteredDropdown,
     PF4MultiSelect,
     Search,
@@ -29,7 +29,7 @@ class FiltersView(BaseLoggedInView):
             'Actions': ActionsDropdown("./div[contains(@class, 'btn-group')]"),
         },
     )
-    pagination = Pagination()
+    pagination = PF4Pagination()
 
     @property
     def is_displayed(self):
