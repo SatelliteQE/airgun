bf16cf5ddb6cfdd5f30db02735e20fb826ad77a5
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2023-11-27 11:36:28 -0500

    added the ui_sesion_id attribute for video-session (#1038) (#1046)
    (cherry picked from commit 92926768d3dfeb16bbb8bcc3c61ca5fce111bf0c)
    
    Co-authored-by: Omkar Khatavkar <okhatavkar007@gmail.com>

--- airgun/session.py
+++ airgun/session.py
@@ -193,6 +193,7 @@ class Session:
         self._login = login
         self.navigator = None
         self.browser = None
+        self.ui_session_id = None
 
     def __call__(self, user=None, password=None, session_cookie=None, url=None, login=None):
         """Stores provided values. This allows tests to provide additional
@@ -273,6 +274,7 @@ class Session:
             self.browser = AirgunBrowser(selenium_browser, self)
             LOGGER.info(f'Session Id For {self.name}: {selenium_browser.session_id}')
             LOGGER.info(f'Setting initial URL to {url}')
+            self.ui_session_id = selenium_browser.session_id
 
             self.browser.url = url
 
