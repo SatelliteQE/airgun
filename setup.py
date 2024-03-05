3b05abc3a9c9d885497490db400122f53005fa7d
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2024-03-05 09:57:47 -0500

    Bump selenium from 4.17.2 to 4.18.1 (#1255) (#1277)
    Bumps [selenium](https://github.com/SeleniumHQ/Selenium) from 4.17.2 to 4.18.1.
    - [Release notes](https://github.com/SeleniumHQ/Selenium/releases)
    - [Commits](https://github.com/SeleniumHQ/Selenium/commits/selenium-4.18.1)
    
    ---
    updated-dependencies:
    - dependency-name: selenium
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    (cherry picked from commit 667aecc200add12a2b2033bf9c8c35d2fd0de6d8)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'pytest',
         'wait_for',
         'webdriver-kaifuku',
-        'selenium==4.17.2',
+        'selenium==4.18.1',
         'widgetastic.core',
         'widgetastic.patternfly',
         'widgetastic.patternfly4',
