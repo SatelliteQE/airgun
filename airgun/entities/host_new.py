a6238d706699ac1953b85b468791c7621a191689
Shweta Singh <shwsingh@redhat.com>
2024-06-19 15:04:56 +0530

    Add Facts View (#1427)

--- airgun/entities/host_new.py
+++ airgun/entities/host_new.py
@@ -5,6 +5,7 @@ from wait_for import wait_for
 
 from airgun.entities.host import HostEntity
 from airgun.navigation import NavigateStep, navigator
+from airgun.views.fact import HostFactView
 from airgun.views.host_new import (
     AllAssignedRolesView,
     EditAnsibleRolesView,
@@ -949,6 +950,15 @@ class NewHostEntity(HostEntity):
         view.insights.remediate.click()
         view = RemediationView(self.browser)
         view.remediate.click()
+
+    def get_host_facts(self, entity_name):
+        view = self.navigate_to(self, 'NewDetails', entity_name=entity_name)
+        self.browser.plugin.ensure_page_safe()
+        view.wait_displayed()
+        self.browser.wait_for_element(view.dropdown, exception=False)
+        view.dropdown.item_select('Facts')
+        host_facts_view = HostFactView(self.browser)
+        return host_facts_view.table.read()
 
 
 @navigator.register(NewHostEntity, 'NewDetails')
