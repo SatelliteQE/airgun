e8f41000c21d6f6c6a374fdd1795282f49e58b10
Sam Bible <sbible@redhat.com>
2024-08-07 21:53:12 -0500

    Adjust all hosts to use menu, add support for Build Management

--- airgun/views/all_hosts.py
+++ airgun/views/all_hosts.py
@@ -1,5 +1,5 @@
 from widgetastic.widget import Checkbox, Text, View
-from widgetastic_patternfly4 import Button, Dropdown
+from widgetastic_patternfly4 import Button, Dropdown, Menu, Radio
 from widgetastic_patternfly4.ouia import (
     PatternflyTable,
 )
@@ -11,12 +11,18 @@ from airgun.views.common import (
 from airgun.views.host_new import ManageColumnsView, PF4CheckboxTreeView
 
 
+class AllHostsMenu(Menu):
+    IS_ALWAYS_OPEN = False
+    BUTTON_LOCATOR = ".//button[contains(@class, 'pf-c-menu-toggle')]"
+    ROOT = f"{BUTTON_LOCATOR}/.."
+
+
 class AllHostsTableView(BaseLoggedInView, SearchableViewMixinPF4):
     title = Text("//h1[normalize-space(.)='Hosts']")
     select_all = Checkbox(
         locator='.//input[@data-ouia-component-id="select-all-checkbox-dropdown-toggle-checkbox"]'
     )
-    bulk_actions = Dropdown(locator='.//div[@data-ouia-component-id="action-buttons-dropdown"]')
+    bulk_actions = AllHostsMenu()
     table_loading = Text("//h5[normalize-space(.)='Loading']")
     no_results = Text("//h5[normalize-space(.)='No Results']")
     manage_columns = Button("Manage columns")
@@ -28,6 +34,7 @@ class AllHostsTableView(BaseLoggedInView, SearchableViewMixinPF4):
             2: Dropdown(locator='.//div[contains(@class, "pf-c-dropdown")]'),
         },
     )
+    alert_message = Text('.//div[@aria-label="Success Alert" or @aria-label="Danger Alert"]')
 
     @property
     def is_displayed(self):
@@ -46,6 +53,27 @@ class HostDeleteDialog(View):
 
     confirm_delete = Button(locator='//button[normalize-space(.)="Delete"]')
     cancel_delete = Button(locator='//button[normalize-space(.)="Cancel"]')
+
+    @property
+    def is_displayed(self):
+        return self.browser.wait_for_element(self.title, exception=False) is not None
+
+
+class BuildManagementDialog(View):
+    """Dialog for Build Management"""
+
+    ROOT = './/div[@data-ouia-component-id="bulk-build-hosts-modal"]'
+
+    title = Text(".//h1[normalize-space(.)='Build management']")
+
+    build = Radio(locator='.//input[@data-ouia-component-id="build-host-radio"]')
+    reboot_now = Checkbox(locator='.//input[@data-ouia-component-id="build-reboot-checkbox"]')
+    rebuild_provisioning_only = Checkbox(
+        locator='.//input[@data-ouia-component-id="rebuild-host-radio"]'
+    )
+
+    confirm = Button(locator='//button[normalize-space(.)="Confirm"]')
+    cancel = Button(locator='//button[normalize-space(.)="Cancel"]')
 
     @property
     def is_displayed(self):
