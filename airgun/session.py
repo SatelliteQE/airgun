a21da4369a8ce6576ac0891e6ede56daf79d2f94
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2024-08-09 12:46:19 -0400

    Add support for File page in Content section of UI (#1426) (#1484)
    * Add support for File page in Content section of UI
    
    * Change Title from H2 to h1
    
    Co-authored-by: Ladislav Vašina <ladislavvasina@gmail.com>
    
    ---------
    
    Co-authored-by: Ladislav Vašina <ladislavvasina@gmail.com>
    (cherry picked from commit e488c2500a0aca04db2f1b214ba1c0371d7435a6)
    
    Co-authored-by: Samuel Bible <sbible@redhat.com>

--- airgun/session.py
+++ airgun/session.py
@@ -37,6 +37,7 @@ from airgun.entities.discoveryrule import DiscoveryRuleEntity
 from airgun.entities.domain import DomainEntity
 from airgun.entities.eol_banner import EOLBannerEntity
 from airgun.entities.errata import ErrataEntity
+from airgun.entities.file import FilesEntity
 from airgun.entities.filter import FilterEntity
 from airgun.entities.hardware_model import HardwareModelEntity
 from airgun.entities.host import HostEntity
@@ -459,6 +460,11 @@ class Session:
     def filter(self):
         """Instance of Filter entity."""
         return self._open(FilterEntity)
+
+    @cached_property
+    def file(self):
+        """Instance of Files entity."""
+        return self._open(FilesEntity)
 
     @cached_property
     def hardwaremodel(self):
