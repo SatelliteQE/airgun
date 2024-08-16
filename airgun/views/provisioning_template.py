c4bce27e4f563086fc85207f0cbb02b3fc096a18
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2024-08-16 03:44:44 -0400

    Table locator updated to get value from "Provisioning Templates" table (#1494) (#1495)

--- airgun/views/provisioning_template.py
+++ airgun/views/provisioning_template.py
@@ -31,7 +31,7 @@ class ProvisioningTemplatesView(BaseLoggedInView, SearchableViewMixinPF4):
     table = Table(
         './/table',
         column_widgets={
-            'Name': Text('.'),
+            'Name': Text('.//a'),
             'Locked': Text('.'),
             'Actions': ActionsDropdown("./div[contains(@class, 'btn-group')]"),
         },
