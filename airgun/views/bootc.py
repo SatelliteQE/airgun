cc12bef61285f1503e051b318facde1d11be6b71
Sam Bible <sbible@redhat.com>
2025-02-19 07:42:20 -0600

    Add view/entity for booted container images page

---
+++ airgun/views/bootc.py
@@ -0,0 +1,33 @@
+from widgetastic.widget import Table, Text, View
+from widgetastic_patternfly4.ouia import ExpandableTable
+
+from airgun.views.common import (
+    BaseLoggedInView,
+    SearchableViewMixinPF4,
+)
+
+
+class BootedContainerImagesView(BaseLoggedInView, SearchableViewMixinPF4):
+    title = Text('.//h1[@data-ouia-component-id="header-text"]')
+
+    # This represents the contents of the expanded table rows
+    class NestedBootCTable(View):
+        table = Table(
+            locator='.//div[@class="pf-c-table__expandable-row-content"]/table',
+            column_widgets={'Image Digest': Text('./a'), 'Hosts': Text('./a')},
+        )
+
+    # Passing in the nested table as content_view, refer to ExpandableTable docs for info
+    table = ExpandableTable(
+        component_id='booted-containers-table',
+        column_widgets={
+            'Image Name': Text('./a'),
+            'Image Digests': Text('./a'),
+            'Hosts': Text('./a'),
+        },
+        content_view=NestedBootCTable(),
+    )
+
+    @property
+    def is_displayed(self):
+        return self.table.is_displayed
