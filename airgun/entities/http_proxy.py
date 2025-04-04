f30015d603c75978f1909c5028ff6500af8e5e61
Vladimir Sedmik <46570670+vsedmik@users.noreply.github.com>
2025-04-04 14:11:50 +0200

    Extend http proxy with Test Connection button (#1787)
    (cherry picked from commit 47e9e3f0f717c3fc362ccbd1704222e839bafaf7)

--- airgun/entities/http_proxy.py
+++ airgun/entities/http_proxy.py
@@ -55,6 +55,14 @@ class HTTPProxyEntity(BaseEntity):
         view.flash.assert_no_error()
         view.flash.dismiss()
 
+    def test_connection(self, entity_name):
+        """Test connection from http-proxy"""
+        view = self.navigate_to(self, 'Edit', entity_name=entity_name)
+        view.test_button.click()
+        view.validations.assert_no_errors()
+        view.flash.assert_no_error()
+        view.flash.dismiss()
+
 
 @navigator.register(HTTPProxyEntity, 'All')
 class ShowAllHTTPProxy(NavigateStep):
