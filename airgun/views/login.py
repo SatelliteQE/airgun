1d8a3e655fa1243d59dc73f62c1550e52056dc50
Jameer Pathan <21165044+jameerpathan111@users.noreply.github.com>
2025-05-07 12:35:28 +0200

    Fix Login page locators for Stream

--- airgun/views/login.py
+++ airgun/views/login.py
@@ -4,8 +4,8 @@ from widgetastic.widget import ClickableMixin, Text, TextInput, View
 class LoginView(View, ClickableMixin):
     username = TextInput(id="login_login")
     password = TextInput(id="login_password")
-    login_text = Text(".//footer[contains(@class,'login-pf-page-footer')]")
-    logo = Text('//img[@alt="logo"]')
+    login_text = Text(".//footer[contains(@class,'pf-v5-c-login__footer')]")
+    logo = Text('//img[@class="pf-v5-c-brand"]')
     submit = Text('//button[@type="submit"]')
 
     @property
