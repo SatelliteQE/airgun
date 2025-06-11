62e325b4be16929d919e39c16c783967d0e2f081
Lukas Hellebrandt <lhellebr@redhat.com>
2025-06-12 01:24:44 +0200

    Change the button to PF5

--- airgun/views/usergroup.py
+++ airgun/views/usergroup.py
@@ -1,6 +1,6 @@
 from widgetastic.widget import Checkbox, Table, Text, TextInput, View
 from widgetastic_patternfly import BreadCrumb
-from widgetastic_patternfly4 import Button as P4Button
+from widgetastic_patternfly5 import Button
 
 from airgun.views.common import BaseLoggedInView, SatTab, SearchableViewMixinPF4
 from airgun.widgets import FilteredDropdown, MultiSelect
@@ -8,7 +8,7 @@ from airgun.widgets import FilteredDropdown, MultiSelect
 
 class UserGroupsView(BaseLoggedInView, SearchableViewMixinPF4):
     title = Text("//h1[normalize-space(.)='User Groups']")
-    new_on_blank_page = P4Button('Create User group')
+    new_on_blank_page = Button('Create User group')
     new = Text("//a[contains(@href, '/usergroups/new')]")
     table = Table(
         './/table',
