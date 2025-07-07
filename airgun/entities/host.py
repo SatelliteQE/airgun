25890a99cf56016e235c43fc498eb31ccb0aca9d
Satellite QE <115476073+Satellite-QE@users.noreply.github.com>
2025-07-07 16:13:22 -0400

    waiting for alert message after host delete (#1891) (#1894)
    (cherry picked from commit 83801dceb53d1f5c77d5e836401584a3c1e0c32b)
    
    Co-authored-by: amolpati30 <151733635+amolpati30@users.noreply.github.com>

--- airgun/entities/host.py
+++ airgun/entities/host.py
@@ -113,6 +113,12 @@ class HostEntity(BaseEntity):
         view.search(entity_name)
         view.table.row(name=entity_name)['Actions'].widget.fill('Delete')
         self.browser.handle_alert()
+        wait_for(
+            lambda: view.flash.assert_message(
+                f"Success alert: Successfully deleted {entity_name}."
+            ),
+            timeout=120,
+        )
         view.flash.assert_no_error()
         view.flash.dismiss()
 
