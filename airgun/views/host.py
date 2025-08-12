47bf576250496e9160041ef7d553572424e876f7
jnagare-redhat <jnagare@redhat.com>
2025-08-12 16:02:40 +0530

    Update airgun/views/host.py
    Co-authored-by: Ladislav Vašina <ladislavvasina@gmail.com>

--- airgun/views/host.py
+++ airgun/views/host.py
@@ -251,7 +251,7 @@ class HostsView(BaseLoggedInView, SearchableViewMixinPF4):
     new_ui_button = Text(".//a[contains(@class, 'btn')][contains(@href, 'new/hosts')]")
     select_all = Checkbox(locator="//input[@id='check_all']")
     table = PF5OUIATable(
-        component_id="table",
+        component_id='table',
         column_widgets={
             0: Checkbox(locator=".//input[@type='checkbox']"),
             'Name': Text(
