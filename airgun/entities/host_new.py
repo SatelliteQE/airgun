fc1cd4118b0b7b4a954ef2f412276bc5f24512a7
Gaurav Talreja <gtalreja@redhat.com>
2025-08-19 03:49:02 +0530

    Initial updates of Vulnerabily views/enties from PR#1883
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>

--- airgun/entities/host_new.py
+++ airgun/entities/host_new.py
@@ -916,6 +916,12 @@ class NewHostEntity(HostEntity):
         wait_for(lambda: view.insights.recommendations_table.is_displayed, timeout=10)
         return view.insights.read()
 
+    def get_cves(self, entity_name):
+        view = self.navigate_to(self, 'NewDetails', entity_name=entity_name)
+        view.wait_displayed()
+        self.browser.plugin.ensure_page_safe()
+        return view.cves.read()
+
     def remediate_with_insights(
         self, entity_name, recommendation_to_remediate=None, remediate_all=False
     ):
