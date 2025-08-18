dfbb8009358d121bf79ec64be120e2a2ca48924f
Gaurav Talreja <gtalreja@redhat.com>
2025-08-19 05:16:29 +0530

    Add view and entities for Hosts->vulnerabilities tab and vulnerability list page
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>

--- airgun/views/host_new.py
+++ airgun/views/host_new.py
@@ -759,14 +759,28 @@ class NewHostDetailsView(BaseLoggedInView):
         pagination = PF5Pagination()
 
     @View.nested
-    class cves(PF5Tab):
+    class vulnerabilities(PF5Tab):
         ROOT = './/div'
 
-        title = Text('//h1[contains(text(), "CVEs tab for host:")]')
+        search_bar = SearchInput(locator='.//input[contains(@aria-label, "search-field")]')
+        cve_menu_toggle = PF5Button(".//button[contains(@class, 'pf-v5-c-menu-toggle')]")
+        no_cves_found_message = Text('.//h5[contains(@class, "pf-v5-c-empty-state__title-text")]')
+
+        vulnerabilities_table = PF5OUIATable(
+            locator='.//table[@data-ouia-component-type="PF5/Table"]',
+            column_widgets={
+                0: PF5Button(locator='.//button[@aria-label="Details"]'),
+                'CVE ID': Text('.//td[contains(@data-label, "CVE ID")]'),
+                'Publish date': Text('.//td[contains(@data-label, "Publish date")]'),
+                'Severity': Text('.//td[contains(@data-label, "Severity")]'),
+                'CVSS base score': Text('.//td[contains(@data-label, "CVSS base score")]'),
+            },
+        )
+        pagination = PF5Pagination()
 
         @property
         def is_displayed(self):
-            return self.title.wait_displayed()
+            return self.vulnerabilities_table.wait_displayed()
 
 
 class InstallPackagesView(View):
