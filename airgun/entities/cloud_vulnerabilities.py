1ead914f705977cd4d0ab997f4efad5db333c699
Gaurav Talreja <gtalreja@redhat.com>
2025-09-11 04:28:15 +0530

    Add new & fix existing helpers for IoP e2e test
    Signed-off-by: Gaurav Talreja <gtalreja@redhat.com>
    (cherry picked from commit cb61cb8865e0c5217f2767e72901077d96f6391c)

--- airgun/entities/cloud_vulnerabilities.py
+++ airgun/entities/cloud_vulnerabilities.py
@@ -2,6 +2,7 @@ from airgun.entities.base import BaseEntity
 from airgun.navigation import NavigateStep, navigator
 from airgun.utils import retry_navigation
 from airgun.views.cloud_vulnerabilities import CloudVulnerabilityView, CVEDetailsView
+from airgun.views.host_new import NewHostDetailsView
 
 
 class CloudVulnerabilityEntity(BaseEntity):
@@ -11,6 +12,13 @@ class CloudVulnerabilityEntity(BaseEntity):
         view = self.navigate_to(self, 'All')
         return view.vulnerabilities_table.read()
 
+    def _navigate_to_cve_details(self, cve_id):
+        """Helper method to navigate to CVE details page"""
+        view = self.navigate_to(self, 'All')
+        view.wait_displayed()
+        view.search_bar.fill(cve_id)
+        view.browser.element(f'.//a[contains(@href, "{cve_id}")]').click()
+
     def get_cve_details(self, cve_id):
         """
         Read CVE details from CVE details page
@@ -18,12 +26,40 @@ class CloudVulnerabilityEntity(BaseEntity):
         Args:
             cve_id (str): CVE ID to get details
         """
-        view = self.navigate_to(self, 'All')
-        view.wait_displayed()
-        view.search_bar.fill(cve_id)
-        view.vulnerabilities_table.row(cve_id)['CVE ID'].click()
+        self._navigate_to_cve_details(cve_id)
         view = CVEDetailsView(self.browser)
         return view.read()
+
+    def get_affected_hosts_by_cve(self, cve_id):
+        """
+        Get list of affected hosts for a specific CVE
+
+        Args:
+            cve_id (str): CVE ID to get affected hosts for
+        """
+        self._navigate_to_cve_details(cve_id)
+        cve_details_view = CVEDetailsView(self.browser)
+        return cve_details_view.affected_hosts_table.read()
+
+    def validate_cve_to_host_details_flow(self, cve_id, hostname=None):
+        """
+        Complete flow: CVE details -> affected hosts -> click host -> host details page
+
+        Args:
+            cve_id (str): CVE ID to test
+            hostname (str, optional): Specific host name to click on.
+        """
+        self._navigate_to_cve_details(cve_id)
+        cve_details_view = CVEDetailsView(self.browser)
+        cve_details_view.search_bar.fill(hostname)
+        cve_details_view.browser.element(f'.//a[contains(text(), "{hostname}")]').click()
+        host_details_view = NewHostDetailsView(self.browser)
+        host_details_view.breadcrumb.wait_displayed()
+        vulnerabilities = getattr(host_details_view.vulnerabilities, 'vulnerabilities_table', None)
+        if vulnerabilities is not None:
+            return vulnerabilities.read()
+        else:
+            return []
 
 
 @navigator.register(CloudVulnerabilityEntity, 'All')
