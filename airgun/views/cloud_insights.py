c7404303525dae556c5cdb2f6da826809ae507f6
Cole Higgins <chiggins@redhat.com>
2025-10-08 19:00:42 -0400

    update indent

--- airgun/views/cloud_insights.py
+++ airgun/views/cloud_insights.py
@@ -87,7 +87,7 @@ class BulkSelectMenuToggle(PF5Menu):
     view.bulk_select.deselect_all() -> to deselect all items using the checkbox
     view.bulk_select.is_all_selected -> to check if all items are selected
     view.bulk_select.items -> to access the menu items
-        (['Select none', 'Select page (1 items)', 'Select all (1 items)'])
+    (['Select none', 'Select page (1 items)', 'Select all (1 items)'])
     view.bulk_select.item_select('Select none') -> to select a specific item by name
     """
 
