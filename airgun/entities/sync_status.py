0450803bed82aea22713908ccd670ba08ea669f9
Vladimir Sedmik <46570670+vsedmik@users.noreply.github.com>
2026-03-31 08:24:30 +0200

    Update SyncStatus page (#2384)
    SyncStatus page has been rewritten to react, this PR reflects the
    changes.

--- airgun/entities/sync_status.py
+++ airgun/entities/sync_status.py
@@ -1,26 +1,26 @@
 from wait_for import wait_for
 
 from airgun.entities.base import BaseEntity
-from airgun.navigation import NavigateStep, navigator
-from airgun.utils import retry_navigation
+from airgun.navigation import NavigateStepWithWait as NavigateStep, navigator
 from airgun.views.sync_status import SyncStatusView
 
 
 class SyncStatusEntity(BaseEntity):
     endpoint_path = '/katello/sync_management'
 
-    def read(self, widget_names=None, active_only=False):
-        """Read all widgets at Sync status entity"""
+    def read(self, widget_names=None, show_syncing_only=False):
+        """Read all widgets at Sync status entity."""
         view = self.navigate_to(self, 'All')
-        view.active_only.fill(active_only)
+        view.show_syncing_only.fill(show_syncing_only)
         return view.read(widget_names=widget_names)
 
     def synchronize(self, repository_paths, synchronous=True, timeout=3600):
         """Synchronize repositories
 
         :param repository_paths: A list of repositories to synchronize
-            where each element of the list is path to repository represented by a list or tuple.
-        :param synchrounous: bool if to wait for all repos sync, defaults to True.
+            where each element of the list is path to repository represented
+            by a list or tuple.
+        :param synchronous: bool if to wait for all repos sync, defaults to True.
         :param timeout: time to wait for all repositories to be synchronized.
 
         Usage::
@@ -29,32 +29,37 @@ class SyncStatusEntity(BaseEntity):
                          ('product1', 'repo2'),
                          ('product2', 'repo2'),
                          ('Red Hat Enterprise Linux Server', '7.5', 'x86_64',
-                          'Red Hat Enterprise Linux 7 Server RPMs x86_64 7.5'),
-                         ('Red Hat Satellite Capsule',
-                          'Red Hat Satellite Capsule 6.2 for RHEL 7 Server RPMs x86_64')])
+                          'Red Hat Enterprise Linux 7 Server RPMs x86_64 7.5')])
 
-        :return: the results text in RESULT columns
+        :return: the results text in Progress / Result columns
         """
         view = self.navigate_to(self, 'All')
-        repo_nodes = [view.table.get_node_from_path(repo_path) for repo_path in repository_paths]
-        for repo_node in repo_nodes:
-            repo_node.fill(True)
-        view.synchronize_now.click()
+        repo_rows = [view.table.get_node_from_path(repo_path) for repo_path in repository_paths]
+        for repo_row in repo_rows:
+            repo_row.select(True)
+        view.synchronize.click()
         if synchronous:
+            # Wait for sync to start (progress bars appear)
             wait_for(
-                lambda: all(node.progress is None for node in repo_nodes),
+                lambda: any(row.has_progress for row in repo_rows),
+                timeout=60,
+                delay=1,
+                logger=view.logger,
+            )
+            # Wait for sync to finish (progress bars disappear)
+            wait_for(
+                lambda: all(not row.has_progress for row in repo_rows),
                 timeout=timeout,
                 delay=5,
                 logger=view.logger,
             )
 
-        return [node.result for node in repo_nodes]
+        return [row.result for row in repo_rows]
 
 
 @navigator.register(SyncStatusEntity, 'All')
-class ShowAllHostCollections(NavigateStep):
+class ShowSyncStatus(NavigateStep):
     VIEW = SyncStatusView
 
-    @retry_navigation
     def step(self, *args, **kwargs):
         self.view.menu.select('Content', 'Sync Status')
