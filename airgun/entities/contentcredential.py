f32596cfd6eb930a43bf5a34914ca044c7f5c4a2
vsedmik <vsedmik@redhat.com>
2026-04-02 14:17:18 +0200

    Update Content Credentials page for React rewrite

--- airgun/entities/contentcredential.py
+++ airgun/entities/contentcredential.py
@@ -1,59 +1,56 @@
-from navmazing import NavigateToSibling
+from wait_for import wait_for
 
 from airgun.entities.base import BaseEntity
-from airgun.navigation import NavigateStep, navigator
-from airgun.utils import retry_navigation
+from airgun.navigation import NavigateStepWithWait as NavigateStep, navigator
 from airgun.views.contentcredential import (
     ContentCredentialCreateView,
     ContentCredentialEditView,
     ContentCredentialsTableView,
+    DeleteContentCredentialModal,
 )
 from airgun.views.product import ProductEditView
 
 
 class ContentCredentialEntity(BaseEntity):
-    endpoint_path = '/content_credentials'
+    # TODO: Update to '/content_credentials' once the React page is moved out of /labs
+    endpoint_path = '/labs/content_credentials'
 
     def create(self, values):
-        """Create new content credentials entity"""
-        view = self.navigate_to(self, 'New')
-        view.fill(values)
-        view.submit.click()
+        """Create new content credentials entity."""
+        raise NotImplementedError(
+            'Content Credential create is not yet implemented in the React UI'
+        )
 
     def delete(self, entity_name):
-        """Delete existing content credentials entity"""
+        """Delete existing content credentials entity via kebab menu."""
         view = self.navigate_to(self, 'Edit', entity_name=entity_name)
-        view.remove.click()
-        self.browser.handle_alert()
-        view.flash.assert_no_error()
-        view.flash.dismiss()
+        view.actions.item_select('Delete')
+        modal = DeleteContentCredentialModal(self.browser)
+        modal.wait_displayed()
+        modal.confirm_delete.click()
 
     def search(self, value):
-        """search for content credentials entity"""
+        """Search for content credentials entity."""
         view = self.navigate_to(self, 'All')
         return view.search(value)
 
     def read(self, entity_name, widget_names=None):
-        """Read content credentials entity values"""
+        """Read content credentials entity values."""
         view = self.navigate_to(self, 'Edit', entity_name=entity_name)
         return view.read(widget_names=widget_names)
 
     def update(self, entity_name, values):
-        """Update content credentials entity values"""
+        """Update content credentials entity values."""
         view = self.navigate_to(self, 'Edit', entity_name=entity_name)
-        filled_values = view.fill(values)
-        view.flash.assert_no_error()
-        view.flash.dismiss()
-        return filled_values
+        return view.fill(values)
 
     def get_product_details(self, entity_name, product_name):
-        """Get entity values for a product which associated to gpg key
+        """Get entity values for a product associated to this content credential.
 
-        :param entity_name: Gpg key name
+        :param entity_name: Content credential name
         :param product_name: Name of associated product
         """
         view = self.navigate_to(self, 'Edit', entity_name=entity_name)
-        view.products.search(product_name)
         view.products.table.row(name=product_name)['Name'].widget.click()
         product_view = ProductEditView(self.browser)
         return product_view.read()
@@ -61,25 +58,25 @@ class ContentCredentialEntity(BaseEntity):
 
 @navigator.register(ContentCredentialEntity, 'All')
 class ShowAllContentCredentials(NavigateStep):
-    """Navigate to All Content Credentials page"""
+    """Navigate to All Content Credentials page."""
 
     VIEW = ContentCredentialsTableView
 
-    @retry_navigation
     def step(self, *args, **kwargs):
-        self.view.menu.select('Content', 'Content Credentials')
+        # TODO: Update menu path to 'Content', 'Content Credentials' once the page is moved out of /labs
+        self.view.menu.select('Lab Features', 'Content Credentials')
 
 
 @navigator.register(ContentCredentialEntity, 'New')
 class AddNewContentCredential(NavigateStep):
-    """Navigate to Create Content Credential page"""
+    """Navigate to Create Content Credential page (stub)."""
 
     VIEW = ContentCredentialCreateView
 
-    prerequisite = NavigateToSibling('All')
-
     def step(self, *args, **kwargs):
-        self.parent.new.click()
+        raise NotImplementedError(
+            'Content Credential create is not yet implemented in the React UI'
+        )
 
 
 @navigator.register(ContentCredentialEntity, 'Edit')
@@ -87,7 +84,7 @@ class EditContentCredential(NavigateStep):
     """Navigate to Content Credential details screen.
 
     Args:
-        entity_name: name of content credential to edit
+        entity_name: name of content credential to view/edit
     """
 
     VIEW = ContentCredentialEditView
@@ -98,4 +95,11 @@ class EditContentCredential(NavigateStep):
     def step(self, *args, **kwargs):
         entity_name = kwargs.get('entity_name')
         self.parent.search(entity_name)
+        wait_for(
+            lambda: self.parent.table.row(name=entity_name),
+            timeout=30,
+            delay=0.5,
+            handle_exception=True,
+            message=f'Waiting for content credential "{entity_name}" row to appear',
+        )
         self.parent.table.row(name=entity_name)['Name'].widget.click()
