93a39d927299f308f31506bb14dcc36bc69d75c5
Adam Růžička <adamruzicka@users.noreply.github.com>
2026-08-04 10:20:20 +0200

    Add cacert text input field to ldap auth source view (#2486)
    * Add cacert text input field to ldap auth source view
    
    * Fix typo

--- airgun/views/ldapauthentication.py
+++ airgun/views/ldapauthentication.py
@@ -51,8 +51,9 @@ class LDAPAuthenticationCreateView(BaseLoggedInView):
 
         name = TextInput(id='auth_source_ldap_name')
         host = TextInput(id='auth_source_ldap_host')
-        text_connection = Text('//a[@id="test_connection_button"]')
+        test_connection = Text('//a[@id="test_connection_button"]')
         ldaps = Checkbox(id='auth_source_ldap_tls')
+        cacert = TextInput(id='auth_source_ldap_cacert')
         port = TextInput(id='auth_source_ldap_port')
         server_type = FilteredDropdown(id='auth_source_ldap_server_type')
 
