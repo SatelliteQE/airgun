80131f094698b56d539d85611f87e4e00fa7549d
Ian Ballou <ianballou67@gmail.com>
2026-09-17 10:50:44 -0400

    SAT-36510 - Add initial modeling for Python repositories (#2528)
    * SAT-36510 - Add initial modeling for Python repositories
    
    (cherry picked from commit 39c5f949327418293f3253d9a0ddf5faa27e6909)
    
    * SAT-36510 - add includes, excludes, and package type for Python

--- airgun/views/repository.py
+++ airgun/views/repository.py
@@ -164,6 +164,29 @@ class RepositoryCreateView(BaseLoggedInView):
         class SpecificHttpProxy(View):
             http_proxy = Select(id='http_proxy')
 
+    @repo_content.register('python')
+    class PythonRepository(View):
+        upstream_url = TextInput(id='url')
+        includes = TextInput(
+            locator=".//label[normalize-space(.)='Includes']/following-sibling::div//textarea"
+        )
+        excludes = TextInput(
+            locator=".//label[normalize-space(.)='Excludes']/following-sibling::div//textarea"
+        )
+        package_types = TextInput(
+            locator=".//label[normalize-space(.)='Package Types']/following-sibling::div//input"
+        )
+        verify_ssl = Checkbox(id='verify_ssl_on_sync')
+        upstream_username = TextInput(id='upstream_username')
+        upstream_password = TextInput(id='upstream_password')
+        download_policy = Select(id='download_policy')
+        http_proxy_policy = Select(id='http_proxy_policy')
+        proxy_policy = ConditionalSwitchableView(reference='http_proxy_policy')
+
+        @proxy_policy.register('Use specific HTTP proxy')
+        class SpecificHttpProxy(View):
+            http_proxy = Select(id='http_proxy')
+
     @repo_content.register('ansible collection')
     class AnsibleCollectionRepository(View):
         arch_restrict = Select(id='architecture_restricted')
@@ -259,6 +282,22 @@ class RepositoryEditView(BaseLoggedInView):
         mirroring_policy = EditableEntrySelect(name='Mirroring Policy')
         upload_content = FileInput(name='content[]')
         upload = Text("//button[contains(., 'Upload')]")
+        proxy_policy = ConditionalSwitchableView(reference='http_proxy_policy')
+
+        @proxy_policy.register(True, default=True)
+        class NoSpecificHttpProxy(View):
+            pass
+
+    @repo_content.register('python')
+    class PythonRepository(View):
+        upstream_url = EditableEntry(name='Upstream URL')
+        includes = EditableEntry(name='Includes')
+        excludes = EditableEntry(name='Excludes')
+        package_types = EditableEntry(name='Package Types')
+        verify_ssl = EditableEntryCheckbox(name='Verify SSL')
+        upstream_authorization = AuthorizationEntry(name='Upstream Authorization')
+        download_policy = EditableEntrySelect(name='Download Policy')
+        http_proxy_policy = EditableEntrySelect(name='HTTP proxy')
         proxy_policy = ConditionalSwitchableView(reference='http_proxy_policy')
 
         @proxy_policy.register(True, default=True)
